c6e4d0fc5d3aca2f4ae277497d0b00f86a8ccb61
Friedrich Lindenberg <friedrich@pudo.org>
2009-11-21 20:54:10 +0100

    [svn r382] recommendations logging to stderr

--- adhocracy/lib/recommendations.py
+++ adhocracy/lib/recommendations.py
@@ -1,10 +1,11 @@
 import math
+import sys
 
 from social import * 
 from karma import *
 
 def log_with_null(n):
-    return math.log(n) if n > 1 else 1
+    return math.log(max(1,n))
 
 def recommend(scope, user, count=5):
     karma_users = delegateable_users(scope)
@@ -12,6 +13,11 @@ def recommend(scope, user, count=5):
     dgb_pop_users = dict(delegateable_popular_agents(scope))
     usr_pop_users = dict(user_popular_agents(user))
     
+    print >>sys.stderr, "KARMA DICT", repr(karma_users)
+    print >>sys.stderr, "DONOR KARMA DICT", repr(donor_karma_users)
+    print >>sys.stderr, "DGB POP DICT", repr(dgb_pop_users)
+    print >>sys.stderr, "USR POP DICT", repr(usr_pop_users)
+        
     users = set(karma_users.keys() + donor_karma_users.keys() + \
                 dgb_pop_users.keys() + usr_pop_users.keys())
     recs = dict()
@@ -22,4 +28,7 @@ def recommend(scope, user, count=5):
                   (donor_karma_users.get(u, 0) * 5) + \
                   (log_with_null(dgb_pop_users.get(u, 0)) * 2) + \
                   (usr_pop_users.get(u, 0) * 3)
-    return sorted(recs.keys(), key=lambda u: recs[u])[0:count]
+    print >>sys.stderr, "RECS DICT", repr(recs)
+    rs = sorted(recs.keys(), key=lambda u: recs[u])[0:count]
+    print >>sys.stderr, "RECS SORTING", repr(rs)
+    return rs
