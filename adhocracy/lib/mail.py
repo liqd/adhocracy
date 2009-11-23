b1ef35fe37e363c78f0f66ff1864c37a89cf1b31
Friedrich Lindenberg <friedrich@pudo.org>
2009-11-23 14:58:28 +0100

    [svn r388] re-do of mailer

--- adhocracy/lib/mail.py
+++ adhocracy/lib/mail.py
@@ -1,27 +1,51 @@
 from datetime import datetime, timedelta
 import sha
 import smtplib
+import logging
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.header import Header
 
 from pylons.i18n.translation import *
 from pylons import session, config, request
+import version
 
-def to_mail(to_name, to_email, subject, body):
+log = logging.getLogger(__name__)
+ENCODING = 'utf-8'
+
+def to_mail(to_name, to_email, subject, body, html_body=None):
     email_from = config['adhocracy.email.from']
     smtp_server = config['smtp_server']
     
-    header = "From: Adhocracy <%s>\r\n" % email_from \
-           + "To: %s <%s>\r\n" % (to_name, to_email) \
-           + "Date: %s\r\n" % datetime.utcnow().ctime() \
-           + "X-Mailer: adhocracy\r\n" \
-           + "Subject: %s\r\n\r\n" % subject 
-    body = _("Hi %s,") % to_name \
-         + "\r\n\r\n%s\r\n\r\n" % body \
-         + _("Cheers,\r\n\r\n    the Adhocracy Team\r\n")
+    
+    body = _(u"Hi %s,") % to_name \
+         + u"\r\n\r\n%s\r\n\r\n" % body \
+         + _(u"Cheers,\r\n\r\n    the Adhocracy Team\r\n")
+    
+    msg = MIMEText(body.encode(ENCODING), 'plain', ENCODING)
+    if html_body:
+        plain_part = MIMEText(body.encode(ENCODING), 
+                              'plain', ENCODING)
+        html_part = MIMEText(html_body.encode(ENCODING), 
+                             'html', ENCODING)
+        msg = MIMEMultipart('alternative')
+        msg.attach(plain_part)
+        msg.attach(html_part)
+    
+    subject = Header(subject.encode(ENCODING), ENCODING)
+    msg['Subject'] = subject
+    msg['From'] = _("Adhocracy <%s>") % email_from
+    to = Header(u"%s <%s>" % (to_name, to_email), ENCODING)
+    msg['To'] = to
+    msg['X-Mailer'] = _("Adhocracy LD %s") % version.get_version()
          
     server = smtplib.SMTP(smtp_server)
     #server.set_debuglevel(1)
-    server.sendmail(email_from, [to_email], header + body)
-    server.quit()
+    #server.sendmail(email_from, [to_email], header + body)
+    #server.quit()
     
-def to_user(to_user, subject, body):
-    return to_mail(to_user.name, to_user.email, subject, body)
+    log.debug("FAKEMAIL\r\n" + msg.as_string())
+    
+def to_user(to_user, subject, body, html_body=None):
+    return to_mail(to_user.name, to_user.email, subject, body, html_body)
