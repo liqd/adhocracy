c286bc0dddffdb1bfbc6beb0c8504abff9065f44
dwt <devnull@localhost>
2009-12-15 00:04:52 +0100

    [svn r423] started splitting the tests into more classes according to what they test and also removed all the time.sleeps as they should not be needed anymore

--- adhocracy/tests/lib/democracy/test_decision.py
+++ adhocracy/tests/lib/democracy/test_decision.py
@@ -10,7 +10,7 @@ from adhocracy.tests import *
 from adhocracy.tests.testtools import *
 
 
-class TestDecision(TestController):
+class TestDecisionWithoutDelegation(TestController):
     
     def setUp(self):
         self.motion = tt_make_motion(voting=True)
@@ -42,8 +42,31 @@ class TestDecision(TestController):
         assert_true(self.decision.is_decided())
         assert_true(self.decision.is_self_decided())
     
+    
+
+class TestDecisionWithDelegation(TestController):
+    
+    def setUp(self):
+        self.motion = tt_make_motion(voting=True)
+        self.poll = Poll(self.motion, self.motion.creator)
+        self.decision = Decision(self.motion.creator, self.poll)
+        self.instance = tt_get_instance()
+        self.user1 = tt_make_user()
+        self.user2 = tt_make_user()
+    
+    def test_can_delegate_to_other_user(self):
+        # TODO: works - but is not pretty
+        delegation = model.Delegation(self.user1, self.user2, self.instance.root)
+        model.meta.Session.add(delegation)
+        model.meta.Session.commit()
+        
+        Decision(self.user2, self.poll).make(model.Vote.AYE)
+        decision = Decision(self.user1, self.poll)
+        assert_equals(decision.result, model.Vote.AYE)
+        assert_equals(len(decision.relevant_votes), 1)
+    
+    # TODO: test that delegations on different scopes interact correctly
     def test_delegation(self):
-        motion = tt_make_motion(voting=True)
         #time.sleep(1)
         instance = tt_get_instance()
         user1 = tt_make_user()
@@ -55,7 +78,6 @@ class TestDecision(TestController):
         model.meta.Session.commit()
         print d1to2
         Decision(user2, motion).make(model.Vote.AYE)
-        time.sleep(1)
         dec = Decision(user1, motion)
         assert dec.result == model.Vote.AYE
         assert len(dec.relevant_votes) == 1   
@@ -64,9 +86,7 @@ class TestDecision(TestController):
         d1to3 = model.Delegation(user1, user3, motion)
         model.meta.Session.add(d1to3)
         model.meta.Session.commit()
-        time.sleep(1)
         Decision(user3, motion).make(model.Vote.NAY)
-        time.sleep(1)
         assert len(dec.relevant_votes) == 1
         assert dec.result == model.Vote.NAY   
              
@@ -76,7 +96,6 @@ class TestDecision(TestController):
         model.meta.Session.commit()
         #print d1to2_2
         Decision(user2, motion).make(model.Vote.NAY)
-        time.sleep(1)
         dec = Decision(user1, motion)
         assert dec.result == model.Vote.NAY
         assert len(dec.relevant_votes) == 2
@@ -92,14 +111,12 @@ class TestDecision(TestController):
                 
         # self override
         Decision(user1, motion).make(model.Vote.ABSTAIN)
-        time.sleep(1)
         dec = Decision(user1, motion)
         assert dec.result == model.Vote.ABSTAIN
         assert len(dec.relevant_votes) == 1
         
     def test_delegation_override(self):
         motion = tt_make_motion(voting=True)
-        time.sleep(1)
         instance = tt_get_instance()
         user1 = tt_make_user()
         user2 = tt_make_user()
@@ -107,15 +124,11 @@ class TestDecision(TestController):
         wide = model.Delegation(user1, user2, instance.root)
         model.meta.Session.add(wide)
         model.meta.Session.commit()
-        time.sleep(1)
         Decision(user2, motion).make(model.Vote.AYE)
-        time.sleep(1)
         small = model.Delegation(user1, user2, motion)
         model.meta.Session.add(small)
         model.meta.Session.commit()
-        time.sleep(1)
         Decision(user2, motion).make(model.Vote.NAY)
-        time.sleep(1)
         dec = Decision(user1, motion)
         
         assert len(dec.relevant_votes) == 1
