0ae5bff9dc39391dfc1c8b1e7d1a1dcdf4834046
Friedrich Lindenberg <friedrich@pudo.org>
2010-01-05 11:34:42 +0100

    [svn r457] remove tests which had no meaning any more and could not serve as a template for better ones

---
+++ adhocracy/tests/lib/democracy/test_state.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+import time 
+
+from adhocracy.tests import *
+from adhocracy.tests.testtools import *
+from nose.tools import *
+
+from adhocracy.lib.democracy import State, Decision
+import adhocracy.model as model
+
+class TestState(TestController):
+    pass
