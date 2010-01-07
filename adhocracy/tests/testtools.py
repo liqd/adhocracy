411e5ff36a4447b37bfeb37c77da10624182a6c0
dwt <devnull@localhost>
2010-01-07 12:16:29 +0100

    [svn r486] updated test-tools methods to conform more closely to usage guidelines of sqlalchemy

--- adhocracy/tests/testtools.py
+++ adhocracy/tests/testtools.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 import random, string, time
 import adhocracy.model as model
 
+#  These functions should all go as convenience functions on the respective models
+
 def tt_get_admin():
     return model.User.find("admin")
 
@@ -15,40 +17,40 @@ def tt_make_str(length=20):
 
 def tt_make_motion(creator=None, voting=False):
     instance = tt_get_instance()
-    if not creator:
+    if creator is None:
         creator = tt_make_user()
     issue = model.Issue(instance, tt_make_str(), creator)
     motion = model.Motion(instance, tt_make_str(), creator)
     motion.parents = [issue]
-    model.meta.Session.add(motion)
-    model.meta.Session.commit()
-    model.meta.Session.refresh(motion)
     
     if voting:
-        # should be Poll.begin_time and Poll.end_time (datetimes)
-        model.poll.begin_time = datetime.now()
-        # tr = model.Transition(motion, model.Motion.STATE_VOTING, creator)
-        # motion.transitions.append(tr)
-        # model.meta.Session.add(motion)
-        # model.meta.Session.commit() 
+        poll = model.Poll(motion, creator)
+        poll.begin_time = datetime.now() - timedelta(hours=1)
+        motion.polls.append(poll)
+        
+    model.meta.Session.add(motion)
+    print 'voting', voting
+    print 'model.meta.Session.dirty', model.meta.Session.dirty
+    print 'model.meta.Session.new', model.meta.Session.new
+    model.meta.Session.flush()
+    print 'poll', poll
+    print 'motion.poll', motion.poll
+    print 'motion.polls', motion.polls
     return motion
 
 def tt_make_user(instance_group=None):
     uname = tt_make_str() 
     user = model.User(uname, u"test@test.test", u"test")
     
-    defgrp = model.Group.by_code(model.Group.CODE_DEFAULT)
-    defmembr = model.Membership(user, None, defgrp)
-    memberships = [defmembr]
-    model.meta.Session.add(defmembr)
+    default_group = model.Group.by_code(model.Group.CODE_DEFAULT)
+    default_membership = model.Membership(user, None, default_group)
+    memberships = [default_membership]
     
     if instance_group:
         instance = tt_get_instance()
-        grpmembr = model.Membership(user, instance, instance_group)
-        model.meta.Session.add(grpmembr)
-        memberships.append(grpmembr)
+        group_membership = model.Membership(user, instance, instance_group)
+        memberships.append(group_membership)
     user.memberships = memberships 
     model.meta.Session.add(user)
-    model.meta.Session.commit()
-    model.meta.Session.refresh(user)
+    model.meta.Session.flush() # write to db and updated db generated attributes
     return user
