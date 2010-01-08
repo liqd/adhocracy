7ef6676d6c9959b7c7a43f1f0e7e32f91b401625
dwt <devnull@localhost>
2010-01-08 18:14:01 +0100

    [svn r515] whitespace fixes so folding works in textmate

--- adhocracy/model/user.py
+++ adhocracy/model/user.py
@@ -115,39 +115,38 @@ class User(Base):
     
     def _set_password(self, password):
         """Hash password on the fly."""
-
+        
         if isinstance(password, unicode):
             password_8bit = password.encode('UTF-8')
         else:
             password_8bit = password
-
+        
         salt = hashlib.sha1(os.urandom(60))
         hash = hashlib.sha1(password_8bit + salt.hexdigest())
         hashed_password = salt.hexdigest() + hash.hexdigest()
-
+        
         if not isinstance(hashed_password, unicode):
             hashed_password = hashed_password.decode('UTF-8')
-
+        
         self._password = hashed_password
-
+    
     def _get_password(self):
         """Return the password hashed"""
         return self._password
-
+    
     password = synonym('_password', descriptor=property(_get_password,
                                                         _set_password))
-
+    
     def validate_password(self, password):
         """
         Check the password against existing credentials.
-
+        
         :param password: the password that was provided by the user to
             try and authenticate. This is the clear text version that we will
             need to match against the hashed one in the database.
         :type password: unicode object.
         :return: Whether the password is valid.
         :rtype: bool
-
         """
         hashed_pass = hashlib.sha1(password + self.password[:40])
         return self.password[40:] == hashed_pass.hexdigest()
@@ -197,4 +196,4 @@ class User(Base):
     def delegate_to_user_in_scope(self, target_user, scope):
         from adhocracy.lib.democracy import DelegationNode
         return DelegationNode.create_delegation(from_user=self, to_user=target_user, scope=scope)
-        
+    
