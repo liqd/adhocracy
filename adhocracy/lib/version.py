f1326219d083a5945ac6d500610c3145d02204ef
Friedrich Lindenberg <friedrich@pudo.org>
2010-01-09 17:11:32 +0100

    [svn r535] test new svn notifications

--- adhocracy/lib/version.py
+++ adhocracy/lib/version.py
@@ -4,14 +4,14 @@ Versioning for the application, especially for the UI.
 
 import re
 
-svn_revision = "$Revision: 534 $"
+svn_revision = "$Revision: 535 $"
 svn_rev_re = re.compile("\$Revision: (\d*) \$")
 
 rev_num = int(svn_rev_re.match(svn_revision).group(1))
 
 REV_TEMPLATE = "2010.1 (r%s)"
 # Modify this:
-# foo schnasel // version bumping 
+# bar // version bumping 
 
 def get_version():
     """ Get a version identifier for use in the public user interface """
