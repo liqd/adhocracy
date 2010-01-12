f5ecc5ee1f110eada0c9409edfd50ba74b720cc9
Friedrich Lindenberg <friedrich@pudo.org>
2010-01-12 14:15:42 +0100

    [svn r570] move deployment template

--- adhocracy/lib/__init__.py
+++ adhocracy/lib/__init__.py
@@ -14,5 +14,6 @@ from recommendations import recommend
 def init_site():
     util.replicate_fallback('static', 'img', 'header_logo.png')
     util.replicate_fallback('static', 'style', 'site.css')
+    util.replicate_fallback('site.wsgi')
     for page in ['index', 'about', 'imprint', 'privacy']:
         util.replicate_fallback('page', page + '.en.html')
