66704d4720e23c681d3c2f76e43451176706b005
dwt <devnull@localhost>
2010-01-15 13:39:24 +0100

    [svn r604] fixed indention to make collapsing in textmate work

--- adhocracy/config/environment.py
+++ adhocracy/config/environment.py
@@ -24,14 +24,14 @@ def load_environment(global_conf, app_conf):
                  controllers=os.path.join(root, 'controllers'),
                  static_files=os.path.join(root, 'static'),
                  templates=[os.path.join(root, 'templates')])
-
+    
     # Initialize config with the basic options
     config.init_app(global_conf, app_conf, package='adhocracy', paths=paths)
-
+    
     config['routes.map'] = make_map()
     config['pylons.app_globals'] = app_globals.Globals()
     config['pylons.h'] = adhocracy.lib.helpers
-
+    
     # Create the Mako TemplateLookup, with the default auto-escaping
     config['pylons.app_globals'].mako_lookup = TemplateLookup(
         directories=paths['templates'],
@@ -39,14 +39,14 @@ def load_environment(global_conf, app_conf):
         module_directory=os.path.join(app_conf['cache_dir'], 'templates'),
         input_encoding='utf-8', default_filters=['escape'],
         imports=['from webhelpers.html import escape'])
-
+    
     # Setup the SQLAlchemy database engine
     engine = engine_from_config(config, 'sqlalchemy.')
     init_model(engine)
-
+    
     # CONFIGURATION OPTIONS HERE (note: all config options will override
     # any Pylons config options)
     init_site()
     init_search()
-    
+
     
