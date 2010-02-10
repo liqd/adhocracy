3b8258b177dc1233eee5a89c922ee78edc29ca1e
Friedrich Lindenberg <friedrich@pudo.org>
2010-02-11 00:32:30 +0100

    [svn r700] refactor queue

--- adhocracy/lib/event/__init__.py
+++ adhocracy/lib/event/__init__.py
@@ -7,22 +7,22 @@ from types import *
 import formatting
 from rss import rss_feed
 import notification
-import queue
+from .. import queue
 
 import adhocracy.model as model
 
 log = logging.getLogger(__name__)
 
+EVENT_QUEUE = 'event'
+
 def emit(event, user, instance=None, topics=[], **kwargs):
-    #return
     event = model.Event(event, user, kwargs, instance=instance)
     event.topics = topics
     model.meta.Session.add(event)
     model.meta.Session.commit()
-    model.meta.Session.refresh(event)
     
     if queue.has_queue():
-        queue.post_event(event)
+        queue.post_message(EVENT_QUEUE, str(event.id))
     else:
         log.warn("Queue failure.")
         process(event)
@@ -34,7 +34,10 @@ def process(event):
     notification.notify(event)
 
 def queue_process():
-    queue.read_events(process)
+    def _process(message):
+        event = model.Event.find(int(message), instance_filter=False)
+        process(event)
+    queue.read_messages(EVENT_QUEUE, _process)
     
 
 
