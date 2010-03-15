984dc3fc1cf88941b462dd88263d60ca89f49a93
Friedrich Lindenberg <friedrich@pudo.org>
2010-03-15 13:51:15 +0100

    [svn r763] rewrite popular_tags query

--- adhocracy/model/tag.py
+++ adhocracy/model/tag.py
@@ -100,12 +100,10 @@ class Tag(object):
         q = meta.Session.query(Tag)
         q = q.add_column(func.count(Tagging.id))
         q = q.join(Tagging)
-        #q = meta.Session.query(Tagging) #[Tagging.tag, func.count(Tagging.tag)])
-        #q = q.add_column(func.count(tagging_table.c.id))
         if ifilter.has_instance():
             q = q.join(Delegateable)
             q = q.filter(Delegateable.instance_id==ifilter.get_instance().id)
-        q = q.group_by(Tag.id)
+        q = q.group_by(Tag.id, Tag.create_time, Tag.name)
         q = q.order_by(func.count(Tagging.id).desc())
         # SQLAlchemy turns this into a fucking subquery:
         #if limit is not None: 
