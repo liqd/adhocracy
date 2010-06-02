2f9a80c57791380c5160463af0f1f873825a5c0f
Friedrich Lindenberg <friedrich@pudo.org>
2010-06-02 12:06:37 +0200

    [svn r1009] smarter HTML diffing

--- adhocracy/lib/text/compare.py
+++ adhocracy/lib/text/compare.py
@@ -1,34 +1,56 @@
 
 from .diff_match_patch import diff_match_patch
 
-def patch_html(text, tag):
-    out = u"<%s>" % tag
-    for ch in text:
-        if ch == u'<':
-            out += u"</%s>" % tag
-        out += ch
-        if ch == u'>':
-            out += u"<%s>" % tag
-    return out + u"</%s>" % tag
+
+class html_diff_generator(object):
+    
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right 
+        self.in_tag = False
+        self.out = u""
+
+    def section(self, text, tag = None):
+        is_open = False
+        if not self.in_tag and tag:
+            is_open = True 
+            self.out += u"<%s>" % tag
+        
+        for ch in text:
+            if ch == u'<':
+                self.in_tag = True
+                if tag and is_open:
+                    self.out += u"</%s>" % tag
+            self.out += ch
+            if ch == u'>':
+                self.in_tag = False
+                if tag and not is_open:
+                    self.out += u"<%s>" % tag
+        
+        if is_open and tag and not self.in_tag:
+            self.out += u"</%s>" % tag
+        
+         
+
 
 def compare_html(left, right):
     dmp = diff_match_patch()
     diffs = dmp.diff_main(left, right)
     #print "STAGE 1", diffs
-    #dmp.diff_cleanupSemantic(diffs)
+    dmp.diff_cleanupSemantic(diffs)
     levenshtein = dmp.diff_levenshtein(diffs) 
     if len(left) > 1 and levenshtein > (max(len(left), len(right)) * 0.5):
         return "<del>%s</del><ins>%s</ins>" % (left, right)
-    full_diff = []
+    full_diff = html_diff_generator(left, right)
     for (op, text) in diffs:
         if op == dmp.DIFF_INSERT:
-            text = patch_html(text, "ins")
+            full_diff.section(text, "ins")
+            
         elif op == dmp.DIFF_DELETE:
-            text = patch_html(text, "del")
-        full_diff.append(text)
-    #print "STAGE 2", diffs
-    #print "STAGE 3", dmp.diff_toDelta(diffs)
-    #print "DIFF", u"".join(full_diff)
-    return u"".join(full_diff)
+            full_diff.section(text, "del")
+        else:
+            full_diff.section(text, None)
+    
+    return full_diff.out
     
     
