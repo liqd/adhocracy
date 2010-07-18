2ca2c8254255fed31db4525e7c1a4f95eef4c7a6
Friedrich Lindenberg <friedrich@pudo.org>
2010-07-18 20:35:16 +0200

    [svn r1118] fix diff for firefox

--- adhocracy/lib/text/lxml_compare.py
+++ adhocracy/lib/text/lxml_compare.py
@@ -1,6 +1,6 @@
 from lxml import etree
 from lxml.html.diff import htmldiff
-from lxml.html import fragment_fromstring
+from lxml.html import fragment_fromstring, tostring
 
 
 def compare_html(left, right):
@@ -9,4 +9,4 @@ def compare_html(left, right):
 def compare_html_sections(left, right):
     diffed_html = htmldiff(left, right)
     root = fragment_fromstring(diffed_html, create_parent=True)
-    return [etree.tostring(c) for c in root.getchildren()]
+    return [tostring(c) for c in root.getchildren()]
