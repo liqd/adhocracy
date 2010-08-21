fbe48a9702a942e4dc103c0dbb9da82badbe9b9e
Friedrich Lindenberg <friedrich@pudo.org>
2010-08-21 17:27:12 +0200

    [svn r1186] fix history view in selection rows

--- adhocracy/model/text.py
+++ adhocracy/model/text.py
@@ -111,6 +111,8 @@ class Text(object):
     @property
     def lines(self):
         from webhelpers.text import truncate
+        if self.text is None:
+            return
         for line in self.text.split("\n"):
             while len(line) > self.LINE_LENGTH:
                 part = truncate(line, length=self.LINE_LENGTH, indicator='', whole_word=True)
