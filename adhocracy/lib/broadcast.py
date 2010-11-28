05a184374e7a232507e18f9cef3a3df1f3ba6f41
Friedrich Lindenberg <friedrich@pudo.org>
2010-11-28 10:12:35 +0100

    [svn r1289] async abuse reporting mechanism

---
+++ adhocracy/lib/broadcast.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+try:
+    import json 
+except ImportError:
+    import simplejson as json
+
+from pylons.i18n.translation import *
+from sqlalchemy import or_
+
+import adhocracy.model as model
+import adhocracy.i18n as i18n
+import queue
+import mail
+
+REPORT_SERVICE = 'report_abuse'
+
+def notify_abuse(instance, user, url, message):
+    message = {
+        'instance': instance.key if instance else None, 
+        'user': user.user_name if user else 'Anonymous',
+        'url': url,
+        'message': message
+        }
+    message = json.dumps(message)
+    if queue.has_queue():
+        queue.post_message(REPORT_SERVICE, message)
+    else:
+        handle_abuse_message(message)
+    
+def get_instance_admins(instance):
+    sgroup = model.Group.find(model.Group.CODE_SUPERVISOR)
+    agroup = model.Group.find(model.Group.CODE_ADMIN)
+    q = model.meta.Session.query(model.User)
+    q = q.join(model.Membership)
+    q = q.filter(model.Membership.instance==instance)
+    q = q.filter(or_(model.Membership.group==sgroup,
+                     model.Membership.group==agroup))
+    q = q.filter(or_(model.Membership.expire_time==None,
+                     model.Membership.expire_time>=datetime.utcnow()))
+    #print q
+    return q.all()
+    
+def get_global_admins():
+    group = model.Group.find(model.Group.CODE_ADMIN)
+    q = model.meta.Session.query(model.User)
+    q = q.join(model.Membership)
+    q = q.filter(model.Membership.instance==None)
+    q = q.filter(model.Membership.group==group)
+    q = q.filter(or_(model.Membership.expire_time==None,
+                     model.Membership.expire_time>=datetime.utcnow()))
+    #print q
+    return q.all()
+    
+def handle_abuse_message(message):
+    message = json.loads(message)
+    admins = []
+    if message.get('instance'):
+        instance = model.Instance.find(message.get('instance'))
+        admins = get_instance_admins(instance)
+    else:
+        admins = get_global_admins()
+    for admin in admins:
+        i18n.user_language(admin)
+        subject = _("Abuse report: %s") % message.get('url')
+        body = _("%(user)s has reported abuse on the page %(url)s:\r\n\r\n%(message)s")
+        body = body % message
+        mail.to_user(admin, subject, body)
+        
