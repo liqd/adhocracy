02faa3313491b2b79c4675c6a4adf3908b53de8a
Carsten Senger <senger@rehfisch.de>
2011-03-04 21:17:43 +0100

    extend poll tests

--- adhocracy/tests/lib/democracy/test_poll.py
+++ adhocracy/tests/lib/democracy/test_poll.py
@@ -1,7 +1,7 @@
 from adhocracy.model import Poll, Vote
 
 from adhocracy.tests import TestController
-from adhocracy.tests.testtools import tt_make_proposal
+from adhocracy.tests.testtools import tt_make_proposal, tt_make_user
 
 
 class TestPoll(TestController):
@@ -12,12 +12,68 @@ class TestPoll(TestController):
         poll.end()
         self.assertTrue(poll.has_ended())
 
-    def test_poll(self):
+    def test_vote_by_creator(self):
+        from adhocracy.lib.democracy import Decision
         proposal = tt_make_proposal(voting=True)
         poll = proposal.polls[0]
         self.assertEqual(0, len(list(poll.votes)))
-        proposal.creator.vote_on_poll(poll, Vote.YES)[0]
+        decision = Decision(proposal.creator, poll)
+        votes = decision.make(Vote.YES)
+        self.assertEqual(len(votes), 1)
+        self.assertEqual(len(decision.votes), 1)
+        self.assertEqual(votes[0].orientation, Vote.YES)
+        self.assertEqual(votes[0].user, proposal.creator)
+
+    def test_second_identical_vote(self):
+        from adhocracy.lib.democracy import Decision
+        proposal = tt_make_proposal(voting=True)
+        poll = proposal.polls[0]
+        self.assertEqual(0, len(list(poll.votes)))
+        decision = Decision(proposal.creator, poll)
+        votes = decision.make(Vote.YES)
+        self.assertEqual(len(votes), 1)
+        self.assertEqual(len(decision.votes), 1)
         self.assertEqual(len(poll.votes), 1)
 
+        # a second, identical vote by the same user
+        decision = Decision(proposal.creator, poll)
+        votes = decision.make(Vote.YES)
+        self.assertEqual(len(votes), 1)
+        self.assertEqual(len(decision.votes), 2)
+        self.assertEqual(len(poll.votes), 2)
+
+    def test_second_revised_vote(self):
+        from adhocracy.lib.democracy import Decision
+        proposal = tt_make_proposal(voting=True)
+        poll = proposal.polls[0]
+        self.assertEqual(0, len(list(poll.votes)))
+        decision = Decision(proposal.creator, poll)
+        votes = decision.make(Vote.YES)
+        self.assertEqual(len(votes), 1)
+        self.assertEqual(len(decision.votes), 1)
+        self.assertEqual(len(poll.votes), 1)
+        self.assertEqual(votes[0].orientation, Vote.YES)
+
+        # a second, revised vote by the same user
+        decision = Decision(proposal.creator, poll)
+        votes = decision.make(Vote.NO)
+        self.assertEqual(len(decision.votes), 2)
+        self.assertEqual(len(poll.votes), 2)
+        self.assertEqual(votes[0].orientation, Vote.NO)
+
+    def test_vote_by_other_user(self):
+        from adhocracy.lib.democracy import Decision
+        proposal = tt_make_proposal(voting=True)
+        poll = proposal.polls[0]
+        self.assertEqual(0, len(list(poll.votes)))
+
+        # make a new user and let him vote
+        new_user = tt_make_user()
+        decision = Decision(new_user, poll)
+        votes = decision.make(Vote.YES)
+        self.assertEqual(len(votes), 1)
+        self.assertEqual(len(decision.votes), 1)
+        self.assertEqual(len(poll.votes), 1)
+        self.assertEqual(votes[0].orientation, Vote.YES)
 
 # REFACT: enable the proposal to be able to have many polls in parallel
