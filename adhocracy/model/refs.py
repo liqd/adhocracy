957c5422c2e087ce66d0592cc37fbd618ff73c1b
Carsten Senger <senger@rehfisch.de>
2011-03-04 14:07:31 +0100

    Cleanup and document .model.refs
    * Add docstrings to the module and api functions.
    * convert the symbols `complex_to_ref` and `complex_to_entities`
      from lambda functions to normal functions to add
      docstrings

--- adhocracy/model/refs.py
+++ adhocracy/model/refs.py
@@ -1,3 +1,17 @@
+'''
+Printable string references for model objects.
+
+Functions to generate string references to model objects
+(`@[<entity_type>:<id>]`) and resolve them back, where
+`entity_type`
+  is a string identifying the model class.
+`id`
+  is an id uniqe for a model instance within the entity_type
+
+The module also provides helpers to work with url encoded
+references and lists or dicts of references or model objects.
+'''
+
 import logging
 import re
 import base64
@@ -50,6 +64,17 @@ def cls_type(cls):
 
 
 def to_ref(entity):
+    '''Generate a string reference to a model object.
+    The reference has the format `@[<entity_type>:<id>]`.
+    Parameters:
+    `entity`
+       An model object that has a method `_index_id()` which returns
+       an uniqe id across all instances of it's model class. The object
+       has to be an instance of one of the model classes in
+       :data:`TYPES`
+    Returns a `unicode` string reference if one can be generated
+    or the passed in `entity` object if not.
+    '''
     for cls in TYPES:
         if isinstance(entity, cls):
             return u"@[%s:%s]" % (entity_type(entity),
@@ -68,6 +93,15 @@ def ref_type(ref):
 
 
 def to_entity(ref, instance_filter=False, include_deleted=True):
+    '''Resolve a model object from a reference in the format
+    `@[<entity_type>:<id>]`.
+    `instance_filter`
+       If `True` limit the search for the model instance to the
+       `current` instance. The instance is determinated by
+       :module:`adhocracy.model.instance_filter`.
+    `include_deleted`
+       If True also resolve to model objects that are already deleted.
+    '''
     match = FORMAT.match(unicode(ref))
     if not match:
         return ref
@@ -83,10 +117,19 @@ def to_entity(ref, instance_filter=False, include_deleted=True):
 
 
 def to_url(entity):
+    '''
+    Generate a reference that encodes the reference to use in urls.
+    See :func:`to_ref`.
+    '''
     return base64.urlsafe_b64encode(str(to_ref(entity)))
 
 
 def from_url(url):
+    '''
+    Resolve an url friendly encoded reference (see :func:`to_url`).
+    See :func:`to_entity`. Note that default parameters of
+    :func:`to_entity` are used.
+    '''
     return to_entity(base64.urlsafe_b64decode(str(url)))
 
 
@@ -103,5 +146,22 @@ def _ify(fun, obj):
         return obj
 
 
-complex_to_refs = lambda obj: _ify(to_ref, obj)
-complex_to_entities = lambda obj: _ify(to_entity, obj)
+def complex_to_refs(obj):
+    '''Generate string references for list or dict containing model
+    objects.
+    `obj`
+       A single model object or a lists or dicts of model objects.
+    Returns a single reference or a list or dict of references.
+    For details see :func:`to_ref`
+    '''
+    return _ify(to_ref, obj)
+
+
+def complex_to_entities(refs):
+    '''Resolve model instances from a list or dict of references.
+    `refs`
+      A list or dict of references.
+    For details see :func:`to_entity'. Note that the default values
+    for the additional parameter of `to_entity` will be used.
+    '''
+    return _ify(to_entity, refs)
