4dc3b5b1290995c64c55adc9f45ded190370af1c
Carsten Senger <senger@rehfisch.de>
2011-03-07 02:56:13 +0100

    cleanup lib.search

--- adhocracy/lib/search/__init__.py
+++ adhocracy/lib/search/__init__.py
@@ -1,30 +1,29 @@
-import logging 
+import logging
 
-import adhocracy.model as model
-import adhocracy.model.hooks as hooks
-from index import *
-import query
+from adhocracy import model
+from adhocracy.lib.search import index
+from adhocracy.model import hooks
+
 
 log = logging.getLogger(__name__)
 
-INDEXED_CLASSES = (model.Proposal, model.Instance, model.User, 
+
+INDEXED_CLASSES = (model.Proposal, model.Instance, model.User,
                    model.Comment, model.Page)
+
 
 def init_search(with_db=True):
     for cls in INDEXED_CLASSES:
-        hooks.register_queue_callback(cls, hooks.POSTINSERT, 
+        hooks.register_queue_callback(cls, hooks.POSTINSERT,
                                       index.update)
-        hooks.register_queue_callback(cls, hooks.POSTUPDATE, 
+        hooks.register_queue_callback(cls, hooks.POSTUPDATE,
                                       index.update)
-        hooks.register_queue_callback(cls, hooks.PREDELETE, 
+        hooks.register_queue_callback(cls, hooks.PREDELETE,
                                       index.delete)
 
+
 def rebuild_all():
-    #clear()
     for cls in INDEXED_CLASSES:
         log.info("Re-indexing %ss..." % cls.__name__)
-        for entity in model.meta.Session.query(cls):            
+        for entity in model.meta.Session.query(cls):
             index.update(entity)
-        
-            
-    
