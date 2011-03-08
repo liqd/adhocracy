c0640b6521d49e07681b0c43c2015c150dff32df
Carsten Senger <senger@rehfisch.de>
2011-03-08 09:48:22 +0100

    Update "can delegate via forward on user" test. Still fails because of permissions

--- adhocracy/tests/model/test_user.py
+++ adhocracy/tests/model/test_user.py
@@ -1,3 +1,5 @@
+from adhocracy.model import Delegation
+
 from adhocracy.tests import TestController
 from adhocracy.tests.testtools import tt_make_proposal, tt_make_user
 
@@ -5,9 +7,12 @@ from adhocracy.tests.testtools import tt_make_proposal, tt_make_user
 class TestUserController(TestController):
 
     def test_can_delegate_via_forward_on_user(self):
+
         proposal = tt_make_proposal(voting=True)
         me = tt_make_user()
         delegate = tt_make_user()
 
-        me.delegate_to_user_in_scope(delegate, proposal)
+        Delegation.create(me, delegate, proposal)
         self.assertEqual(delegate.number_of_votes_in_scope(proposal), 2)
+        # fixme: atm that fails cause the user does not have the
+        # vote.cast permission.
