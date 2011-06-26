bd3e882f875c9f05d356e4e8555b33e88262ef6b
Carsten Senger <senger@rehfisch.de>
2011-06-26 19:17:38 +0200

    cleanup

--- adhocracy/lib/tiles/__init__.py
+++ adhocracy/lib/tiles/__init__.py
@@ -2,28 +2,29 @@ import logging
 
 from adhocracy import model
 
-import instance_tiles as instance
 import comment_tiles as comment
-import user_tiles as user
-import proposal_tiles as proposal
-import event_tiles as event
 import decision_tiles as decision
 import delegation_tiles as delegation
-import revision_tiles as revision
+import event_tiles as event
+import instance_tiles as instance
+import milestone_tiles as milestone
 import page_tiles as page
 import poll_tiles as poll
+import proposal_tiles as proposal
+import revision_tiles as revision
+import selection_tiles as selection
 import tag_tiles as tag
 import text_tiles as text
-import selection_tiles as selection
-import milestone_tiles as milestone
-
+import user_tiles as user
 
 log = logging.getLogger(__name__)
+
 
 def dispatch_row_with_comments(entity):
     if isinstance(entity, model.Comment):
         return comment.row(entity)
     return dispatch_row(entity)
+
 
 def dispatch_row(entity):
     if isinstance(entity, model.User):
@@ -40,6 +41,5 @@ def dispatch_row(entity):
     elif isinstance(entity, model.Tag):
         return tag.row(entity)
     else:
-        pass 
+        pass
         #log.warn("WARNING: Cannot render %s!" % repr(entity))
-
