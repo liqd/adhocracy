0b7b5e77b60febf9df566054a2747458bdb5bac8
Carsten Senger <senger@rehfisch.de>
2011-06-26 19:04:43 +0200

    fix missing quotes for alt= in  user link, cleanup

--- adhocracy/lib/helpers/user_helper.py
+++ adhocracy/lib/helpers/user_helper.py
@@ -1,5 +1,5 @@
-import cgi 
-import urllib 
+import cgi
+import urllib
 import hashlib
 
 from pylons import tmpl_context as c
@@ -16,20 +16,22 @@ def icon_url(user, size=32):
     id = user.email if user.email else user.user_name
     gravatar_url = "https://secure.gravatar.com/avatar.php?"
     gravatar_url += urllib.urlencode({
-        'gravatar_id': hashlib.md5(id.strip().lower()).hexdigest(), 
-        'default': 'identicon', 
+        'gravatar_id': hashlib.md5(id.strip().lower()).hexdigest(),
+        'default': 'identicon',
         'size': str(size)})
     return gravatar_url
 
 
 def link(user, size=16, scope=None):
-    if user.delete_time: 
+    if user.delete_time:
         return _("%s (deleted user)") % user.name
 
     @cache.memoize('user_generic_link')
     def _generic_link(user, instance, size, scope):
-        _url = u"<a href='%s' class='user_link'><img width='16' height='16' class='user_icon' src='%s' alt="" /> %s</a>" % (
-            url(user), icon_url(user, size=size), cgi.escape(user.name))
+        _url = (u'<a href="%s" class="user_link"><img width="16" '
+                'height="16" class="user_icon" src="%s" '
+                'alt="" /> %s</a>') % (url(user), icon_url(user, size=size),
+                                       cgi.escape(user.name))
         if scope and ((not c.instance) or c.instance.allow_delegate):
             votes = user.number_of_votes_in_scope(scope)
             if votes > 0:
@@ -44,12 +46,14 @@ def link(user, size=16, scope=None):
             dnode = democracy.DelegationNode(other, scope)
             for delegation in dnode.outbound():
                 if delegation.agent == user:
-                    icon = u"<img class='user_icon' width='16' height='16' src='/img/icons/delegate_16.png' />"
-                    url += u"<a href='%s'>%s</a>" % (entity_url(delegation), icon)
+                    icon = (u'<img class="user_icon" width="16" height="16" '
+                            'src="/img/icons/delegate_16.png" />')
+                    url += u'<a href="%s">%s</a>' % (entity_url(delegation),
+                                                     icon)
         return url
 
     return _specific_link(user, c.instance, size, scope, c.user)
-    
+
 
 def url(user, instance=None, **kwargs):
     @cache.memoize('user_url')
@@ -63,10 +67,10 @@ def url(user, instance=None, **kwargs):
 def bc_entity(user):
     return _url.BREAD_SEP + _url.link(user.name, url(user))
 
+
 def breadcrumbs(user):
     bc = _url.root()
     bc += _url.link(_("Users"), u'/user')
     if user is not None:
         bc += bc_entity(user)
     return bc
-    
