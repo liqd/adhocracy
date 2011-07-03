18c5c1dc317731196e17a3c1a6a2e024ebd52ce0
Carsten Senger <senger@rehfisch.de>
2011-07-03 23:40:30 +0200

    cleanup, remove initial_size=3 for user pager introduced in 354b20fcbae4

--- adhocracy/lib/pager.py
+++ adhocracy/lib/pager.py
@@ -2,27 +2,28 @@ import urllib
 import math
 import logging
 
-import formencode
-from formencode import foreach, validators, htmlfill, Invalid
-
+from formencode import validators
 from pylons.i18n import _
-from pylons import request, response, tmpl_context as c
+from pylons import request, tmpl_context as c
 
-from templating import render_def
-import sorting
-import tiles
-
+from adhocracy.lib.templating import render_def
+from adhocracy.lib import sorting, tiles
 
 log = logging.getLogger(__name__)
 
-class NamedPager(object): 
+PAGE_VALIDATOR = validators.Int(min=1, not_empty=True)
+SIZE_VALIDATOR = validators.Int(min=1, max=250, not_empty=True)
+
+
+class NamedPager(object):
     """
     A ``NamedPager`` is a list generator for the UI. The ``name`` is required
-    in order to distinguish multiple pagers working on the same page.  
+    in order to distinguish multiple pagers working on the same page.
     """
-    
-    def __init__(self, name, items, itemfunc, initial_size=10, size=None, sorts={}, 
-                 default_sort=None, enable_sorts=True, enable_pages=True, **kwargs):
+
+    def __init__(self, name, items, itemfunc, initial_size=10,
+                 size=None, sorts={}, default_sort=None, enable_sorts=True,
+                 enable_pages=True, **kwargs):
         self.name = name
         self._items = items
         self.itemfunc = itemfunc
@@ -31,7 +32,7 @@ class NamedPager(object):
             self.size = size
         elif c.user and c.user.page_size:
             self.size = c.user.page_size
-        else: 
+        else:
             self.size = initial_size
         self.sorts = sorts
         if len(sorts.values()):
@@ -43,47 +44,44 @@ class NamedPager(object):
         self.enable_pages = enable_pages
         self.kwargs = kwargs
         self._parse_request()
-     
-      
+
     def _parse_request(self):
         try:
-            page_val = validators.Int(min=1, not_empty=True)
-            self.page = page_val.to_python(request.params.get("%s_page" % self.name))
-        except: 
+            page_value = request.params.get("%s_page" % self.name)
+            self.page = PAGE_VALIDATOR.to_python(page_value)
+        except:
             self.page = 1
-        
+
         try:
-            size_val = validators.Int(min=1, max=250, not_empty=True)
-            self.size = size_val.to_python(request.params.get("%s_size" % self.name))       
+            size_value = request.params.get("%s_size" % self.name)
+            self.size = SIZE_VALIDATOR.to_python(size_value)
         except:
             pass
-        
+
         try:
-            sort_val = validators.Int(min=1, max=len(self.sorts.keys()), not_empty=True)
-            self.selected_sort = sort_val.to_python(request.params.get("%s_sort" % self.name))
-        except: 
+            sort_validator = validators.Int(min=1, max=len(self.sorts.keys()),
+                                            not_empty=True)
+            sort_value = request.params.get("%s_sort" % self.name)
+            self.selected_sort = sort_validator.to_python(sort_value)
+        except:
             pass
-    
-    
-    @property        
+
+    @property
     def items(self):
         if not self.sorted and len(self.sorts.values()):
             sorter = self.sorts.values()[self.selected_sort - 1]
             self._items = sorter(self._items)
             self.sorted = True
-        return self._items[self.offset:self.offset+self.size]
-    
-    
+        return self._items[self.offset:self.offset + self.size]
+
     @property
     def offset(self):
-        return (self.page-1)*self.size
-    
-    
+        return (self.page - 1) * self.size
+
     @property
     def pages(self):
-        return int(math.ceil(len(self._items)/float(self.size)))
-    
-    
+        return int(math.ceil(len(self._items) / float(self.size)))
+
     def serialize(self, page=None, size=None, sort=None, **kwargs):
         query = dict(request.params.items())
         query.update(self.kwargs)
@@ -91,23 +89,19 @@ class NamedPager(object):
         query["%s_page" % self.name] = page if page else 1
         query["%s_size" % self.name] = size if size else self.size
         query["%s_sort" % self.name] = sort if sort else self.selected_sort
-        
+
         query = dict([(str(k), unicode(v).encode('utf-8')) \
                       for k, v in query.items()])
         return "?" + urllib.urlencode(query.items())
-    
-    
+
     def here(self):
         return render_def('/pager.html', 'namedpager', pager=self)
-    
-    
+
     def to_dict(self):
         return self.items
-    
-        
+
     def __len__(self):
         return len(self.items)
-
 
 
 def instances(instances):
@@ -118,11 +112,11 @@ def instances(instances):
                              _("alphabetically"): sorting.delegateable_label},
                       default_sort=sorting.instance_activity)
 
-  
+
 def proposals(proposals, default_sort=None, **kwargs):
-    if default_sort is None: 
+    if default_sort is None:
         default_sort = sorting.proposal_mixed
-    sorts = {#_("oldest"): sorting.entity_oldest,
+    sorts = {  # _("oldest"): sorting.entity_oldest,
              _("newest"): sorting.entity_newest,
              _("newest comment"): sorting.delegateable_latest_comment,
              _("support"): sorting.proposal_support,
@@ -131,16 +125,18 @@ def proposals(proposals, default_sort=None, **kwargs):
     return NamedPager('proposals', proposals, tiles.proposal.row, sorts=sorts,
                       default_sort=default_sort, **kwargs)
 
+
 def milestones(milestones, default_sort=None, **kwargs):
-    if default_sort is None: 
+    if default_sort is None:
         default_sort = sorting.milestone_time
-    sorts = {#_("oldest"): sorting.entity_oldest,
+    sorts = {  # _("oldest"): sorting.entity_oldest,
              _("by date"): sorting.milestone_time,
              _("newest"): sorting.entity_newest,
              _("oldest"): sorting.entity_oldest,
              _("alphabetically"): sorting.delegateable_title}
-    return NamedPager('milestones', milestones, tiles.milestone.row, sorts=sorts,
-                      default_sort=default_sort, **kwargs)
+    return NamedPager('milestones', milestones, tiles.milestone.row,
+                      sorts=sorts, default_sort=default_sort, **kwargs)
+
 
 def pages(pages, detail=True, default_sort=None, **kwargs):
     if default_sort is None:
@@ -153,27 +149,26 @@ def pages(pages, detail=True, default_sort=None, **kwargs):
     return NamedPager('pages', pages, tiles.page.row, sorts=sorts,
                     default_sort=default_sort, **kwargs)
 
-      
+
 def users(users):
-    sorts={_("oldest"): sorting.entity_oldest,
-           _("newest"): sorting.entity_newest,
-           _("activity"): sorting.user_activity,
-           _("alphabetically"): sorting.user_name}
-                
+    sorts = {_("oldest"): sorting.entity_oldest,
+             _("newest"): sorting.entity_newest,
+             _("activity"): sorting.user_activity,
+             _("alphabetically"): sorting.user_name}
+
     return NamedPager('users', users, tiles.user.row, sorts=sorts,
-                      initial_size=3,
                       default_sort=sorting.user_activity)
 
-  
+
 def user_decisions(decisions):
-    return NamedPager('decisions', decisions, tiles.decision.user_row, 
+    return NamedPager('decisions', decisions, tiles.decision.user_row,
                       sorts={_("oldest"): sorting.entity_oldest,
                              _("newest"): sorting.entity_newest},
                       default_sort=sorting.entity_newest)
 
 
 def scope_decisions(decisions):
-    return NamedPager('decisions', decisions, tiles.decision.scope_row, 
+    return NamedPager('decisions', decisions, tiles.decision.scope_row,
                       sorts={_("oldest"): sorting.entity_oldest,
                              _("newest"): sorting.entity_newest},
                       default_sort=sorting.entity_newest)
@@ -185,7 +180,7 @@ def comments(comments):
                              _("newest"): sorting.entity_newest},
                       default_sort=sorting.entity_newest)
 
-   
+
 def delegations(delegations):
     return NamedPager('delegations', delegations, tiles.delegation.row,
                       sorts={_("oldest"): sorting.entity_oldest,
