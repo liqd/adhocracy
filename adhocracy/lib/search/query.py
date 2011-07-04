cb61330fd6c70e103973e878658842b4c1b3fdf4
Carsten Senger <senger@rehfisch.de>
2011-07-04 15:04:41 +0200

    Add get_sunburnt_connection() and sunburnt_query(entity_type)
    sunburnt_query return a sunbunt query object that might be precofigured
    with a filter for the entity_type given

--- adhocracy/lib/search/query.py
+++ adhocracy/lib/search/query.py
@@ -1,9 +1,38 @@
 import logging
 
-from index import get_connection
+from httplib2 import Http
+from sunburnt import SolrInterface
+
+from adhocracy.lib.search.index import get_connection
 from adhocracy.model import refs
 
 log = logging.getLogger(__name__)
+
+
+def get_sunburnt_connection():
+    from pylons import config
+    solr_url = config.get('adhocracy.solr.url', 'http://localhost:8983/solr/')
+    solr_url = solr_url.strip()
+    if not solr_url.endswith('/'):
+        solr_url = solr_url + '/'
+    print solr_url
+    http_connection = Http()
+
+    return SolrInterface(solr_url, http_connection=http_connection,
+                         mode='r')
+
+
+def sunburnt_query(entity_type=None):
+    '''
+    return a sunburnt query object. If *entity_type* is given,
+    return a query object preconfigured to only fetch documents
+    from solr with a matching doc_type
+    '''
+    si = get_sunburnt_connection()
+    q = si.query()
+    if entity_type:
+        q = q.query(doc_type=refs.cls_type(entity_type))
+    return q
 
 
 def run(terms, instance=None, entity_type=None, **kwargs):
