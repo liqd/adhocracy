bcce07970c158c484de50b0ff20202019a9f662e
Carsten Senger <senger@rehfisch.de>
2011-07-16 02:27:07 +0200

    First innocent functional test for the root page

--- adhocracy/tests/functional/test_root.py
+++ adhocracy/tests/functional/test_root.py
@@ -2,4 +2,18 @@ from adhocracy.tests import TestController
 
 
 class TestRootController(TestController):
-    pass
+
+    def test_root(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status, '200 OK')
+
+    def test_root_has_timeline(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status, '200 OK')
+        self.assertTrue('simile-ajax-api.js' in response.body)
+
+    def test_root_has_login_link(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status, '200 OK')
+        self.assertTrue('<a href="/login">' in response.body)
+        self.assertTrue('<a href="/register">' in response.body)
