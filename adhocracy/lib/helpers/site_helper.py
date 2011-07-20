e97ba6c80671f7cfaefa752a3f95ec14ee1c7da8
Joscha Krutzki <joka@jokasis.de>
2011-07-20 15:04:13 +0200

    dont generate links with portnumber - @carsten warum wurde das rausgenommen?

--- adhocracy/lib/helpers/site_helper.py
+++ adhocracy/lib/helpers/site_helper.py
@@ -11,9 +11,6 @@ def base_url(instance, path=None):
     if instance is not None and g.single_instance is None:
         url += instance.key + "."
     url += request.environ.get('adhocracy.domain')
-    port = int(request.environ.get('SERVER_PORT'))
-    if port != 80:
-        url += ':' + str(port)
     if path is not None:
         url += path
     return url
