40ca4a951da7172448980cd6d187cb71cf055ba6
Carsten Senger <senger@rehfisch.de>
2011-07-26 00:04:58 +0200

    emacs: .dir-locals.el to set nxml-mode and html-mode indentation to 4 spaces

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;; to use this made the variables local
+;; (make-local-variable 'nxml-child-indent)
+;; (make-local-variable 'nxml-outline-child-indent)
+((nxml-mode . ((nxml-child-indent . 4)
+               (nxml-outline-child-indent . 4)))
+ (html-mode . ((sgml-basic-offset . 4)))
+ )
