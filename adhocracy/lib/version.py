275690f31ba94b197dea4748472434828b6db132
Carsten Senger <senger@rehfisch.de>
2011-08-01 15:50:31 +0200

    remove pdb call

--- adhocracy/lib/version.py
+++ adhocracy/lib/version.py
@@ -7,5 +7,4 @@ from pkg_resources import get_distribution
 
 def get_version():
     """ Get a version identifier for use in the public user interface """
-    import pdb; pdb.set_trace()
     return get_distribution("adhocracy").version
