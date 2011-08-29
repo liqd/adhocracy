5f977ee57da759d64a27e76994e593932d4ce69a
Joscha Krutzki <joka@jokasis.de>
2011-08-29 21:24:07 +0200

    added badge tile

--- adhocracy/lib/tiles/badge_tiles.py
+++ adhocracy/lib/tiles/badge_tiles.py
@@ -1,3 +1,8 @@
+def badge(badge):
+    from adhocracy.lib.templating import render_def
+    return render_def('/badge/tiles.html', 'badge', badge=badge,
+                      cached=True)     
+
 def badges(badges):
     from adhocracy.lib.templating import render_def
     return render_def('/badge/tiles.html', 'badges', badges=badges,
