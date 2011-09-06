c44c16e8ed68658eff1d36c3655b01e687499cae
Joscha Krutzki <joka@jokasis.de>
2011-09-06 20:31:10 +0200

    updated version and changelog

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 
 setup(
     name='adhocracy',
-    version='1.2dev',
+    version='1.2a4dev',
     description='Policy drafting and decision-making web platform',
     author='Liquid Democracy e.V.',
     author_email='info@liqd.net',
