28eb6039996c28ef98b177d9d78050080d132ed3
Carsten Senger <senger@rehfisch.de>
2011-10-12 15:18:56 +0200

    Add tiles.poll.widget to render a rating widget with several states
    This renders the rating widget used for comments (small),
    selections/proposed implementations (small) and proposals (big; not
    the voting box for proposals during the currently possible "official"
    voting phase, which is tiles.poll.booth).
    
    It has preliminary support to render the widget deactivated with only the
    count activated.
    
    What's missing atm:
    * FIXME: Render vote_up/vote_down as span's if the user already has that
      position.
    * FIXME: Render the vote_count as a link to the votes listing
    * Implement usefull tooltips like 'You need to login' or 'Rating phase
      ended'.
    * Support Voting (oposite to rating) in the PollTile class.

--- adhocracy/lib/tiles/poll_tiles.py
+++ adhocracy/lib/tiles/poll_tiles.py
@@ -1,18 +1,62 @@
 from pylons import tmpl_context as c
 
 from adhocracy import model
-from adhocracy.lib import democracy
+from adhocracy.lib import democracy, helpers as h
 from adhocracy.lib.tiles import comment_tiles, proposal_tiles
 from adhocracy.lib.tiles.util import render_tile, BaseTile
 
 
 class PollTile(BaseTile):
+    '''
+    A class to write poll tiles with helper methods for the template.
+    '''
+    RATE = 0
+    VOTE = 1
 
-    def __init__(self, poll):
+    def __init__(self, poll, deactivated=False, need_auth=False):
         self.poll = poll
+        self.deactivated = deactivated
+        self.need_auth = need_auth
         self.__state = None
         self.__decision = None
         self.__dnode = None
+        self.html_id = 'poll%s' % poll.id
+        score = poll.tally.score
+        self.count_class = ('positive' if score > 0 else 'negative' if
+                            score < 0 else 'neutral')
+        self.display_score = u'0' if score == 0 else u"%+d" % score
+        self.login_redirect_url = h.login_redirect_url(poll.scope, anchor=self.html_id)
+
+    def action_url(self, position, type_=RATE):
+        '''
+        Generate an url to rate or vote in a poll.
+
+        *position*
+            The value of the vote. +1 = Pro, 0 = Neutral, +1 = Con.
+            For *type_* 'rate' it's only +1 and -1
+        *type_*
+            The type to use the poll. Either ``RATE`` (default)
+            or ``VOTE``. ``RATE`` is a three step variant
+            where the user steps from -1 to 0 to +1 or the other
+            way around. ``VOTE`` will directly set the selected position
+            and is NotImplemented.
+        '''
+        if self.need_auth:
+            return self.login_redirect_url
+        elif self.deactivated:
+            return ''
+        if type_ == self.RATE:
+            if position == 0:
+                raise ValueError('Rating neutral is not possible')
+            return self._rate_url(self.poll, 1)
+        elif type_ == self.VOTE:
+            raise NotImplemented('PollTile does not support action_url for voting')
+        else:
+            raise ValueError('Unsupported type_, use VOTE or RATE')
+
+    @property
+    def deactivated_class(self):
+        return 'deactivated' if self.deactivated else ''
 
     @property
     def state(self):
@@ -57,10 +101,54 @@ class PollTile(BaseTile):
     def result_dissent(self):
         return round(self.poll.tally.rel_against * 100.0, 1)
 
+    @property
+    def current_position(self):
+        '''
+        Return the decision of the current user
+        This will be
+        *None*
+            if there is no user
+        *-1, 0 or 1 (int)*
+            against, abstain/not voted and affirmation
+        '''
+        decision = self.decision
+        if decision:
+            return decision.result
+
+    def _rate_url(self, poll, position):
+        params = {'url': h.entity_url(poll, member='rate'),
+                  'token_param': h.url_token(),
+                  'position': position}
+        return "%(url)s?position=%(position)s&%(token_param)s" % params
+
 
 def booth(poll):
     return render_tile('/poll/tiles.html', 'booth',
                         PollTile(poll), poll=poll, user=c.user, cached=True)
+
+
+def widget(poll, cls='', deactivated=False, need_auth=False):
+    '''
+    Render a rating widget for an :class:`adhocracy.model.poll.Poll`
+    TODO: Add support for helpful tooltips for the voting buttons.
+
+    *cls* (str)
+        The type that will be rendered as css class in the widget.
+        By default it is a small widget. Using 'big' will render a
+        big one.
+    *deactivated*
+        Render the widget deactivated which does not show vote buttons
+        or the current position of the user, but still the vote count.
+    *need_auth*
+       Render the widget in a semi-active state where users will be
+       redirected to the login form. TODO: Implement useful tooltips.
+       
+    '''
+    t = PollTile(poll, deactivated, need_auth)
+    return render_tile('/poll/tiles.html', 'widget',
+                       t, poll=poll, user=c.user, cls=cls,
+                       deactivated=deactivated, need_auth=need_auth,
+                       cached=True)
 
 
 def header(poll, active=''):
