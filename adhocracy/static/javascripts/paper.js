c8d221f51c4b2fe83f7db59f21b26b34d84ccf48
Carsten Senger <senger@rehfisch.de>
2011-11-13 18:00:05 +0100

    Refactor PaperModel js
    * Rename to PaperModel.
    * Make it a constructor function.
    * move the variants into a subproperty variants. This way we can
      subclass the model and reuse the template bindings.
    * use camleCase or CapitalizedCamelCase for properties.
    * Put the model class into the 'adhocracy' namespace.
    * Add documentation.
    * Adapt templates.

--- adhocracy/static/javascripts/paper.js
+++ adhocracy/static/javascripts/paper.js
@@ -1,49 +1,119 @@
 /*jslint vars:true, browser:true, nomen:true */
-/*global document:true, jQuery:true, $:true */
+/*global document:true, jQuery:true, $:true, ko:true */
 
-var viewModel = {
-    variants: {}, // cache variants
-    switch_diff: ko.observable(true), // state of the changes_switch selector
-    current: ko.mapping.fromJS(init_variant), // the current paper
-    // show a certain *variant*. Load the data with
-    // ajax from the *href* if it is not in the cache
-    load: function (variant, href) {
-        var exists = false,
-            model = this,
-            update_model;
 
-        update_model = function () {
-            ko.mapping.fromJS(model.variants[variant], model.current);
+/**
+ * Javascript application code for adhocracy
+ *
+ * @module adhocracy
+ *
+ */
+
+// Make sure we have an "adhocracy" namespace.
+var adhocracy;
+if (typeof (adhocracy) === "undefined") {
+    adhocracy = {};
+}
+
+(function () {
+
+    "use strict";
+
+    /**
+     * @namespace adhocracy
+     * @class PaperModel
+     */
+    adhocracy.PaperModel = function () {
+        this.variants = {
+            /**
+             * Cache for variant data
+             *
+             * @property cache
+             * @type object
+             * @protected
+             */
+            cache: {},
+            /**
+             * Enable/disable diff display mode. The final decicion if a
+             * diff or plain text is shown is made in .showText().
+             *
+             * @property switchDiff
+             * @type bool (observable)
+             * @default
+             */
+            switchDiff: ko.observable(true),
+            /**
+             * The current
+             * @property switchDiff
+             * @type object (with observeable properties)
+             * @default
+             */
+            current: undefined,
+            init: function (data) {
+                var variant = data.variant;
+                this.cache[variant] = data;
+                this.current = ko.mapping.fromJS(data);
+                this.load(variant);
+            },
+            /**
+             * Load data from a certain variant from a url and
+             * display it. If the data is in the cache, the
+             * url is ignored.
+             *
+             * @method load
+             * @param {String} variant The name of the variant
+             * @param {String} url The url to load data from
+             * @return {undefined}
+             */
+            load: function (variant, url) {
+                var self = this,
+                    update_model;
+
+                update_model = function () {
+                    ko.mapping.fromJS(self.cache[variant], self.current);
+                };
+
+                if (this.cache[variant] !== undefined) {
+                    update_model();
+                } else {
+                    $.getJSON(url,
+                        {variant_json: '1'},
+                        function (data) {
+                            self.cache[data.variant] = data;
+                            update_model();
+                        });
+                }
+            },
+            /**
+             * Tell if the plain or the diff text should be shown.
+             *
+             * @method showText
+             * @return {String} 'plain' or 'diff'
+             */
+            showText: function () {
+                if (this.current.is_head()) {
+                    return 'plain';
+                }
+                if (this.switchDiff()) {
+                    return 'diff';
+                }
+                return 'plain';
+            },
+            /**
+             * Convenience shortcut to .showText() === 'diff'
+             *
+             * @method showDiff
+             * @return {Boolean}
+             */
+            showDiff: function () { return this.showText() === 'diff'; },
+            /**
+             * Convenience shortcut to .showText() === 'plain'
+             *
+             * @method showPlain
+             * @return {Boolean}
+             */
+            showPlain: function () { return this.showText() === 'plain'; }
         };
+    };
 
-        if (this.variants[variant] !== undefined) {
-            update_model();
-        } else {
-            $.getJSON(href, {variant_json: '1'},
-                      function(data) {
-                          model.variants[data.variant] = data;
-                          update_model();
-                      });
-        }
-    },
-    // Decide how the paper will be displayed.
-    // Returns 'plain' or 'diff' depending on the state of the
-    // diff switcher and the selected variant.
-    show: function () {
-        if (this.current.is_head()) {
-            return 'plain';
-        }
-        if (this.switch_diff()) {
-            return 'diff';
-        }
-        return 'plain';
-    },
-    // convenience functions
-    show_diff: function () { return this.show() === 'diff'; },
-    show_plain: function () { return this.show() === 'plain'; }
-};
-
-// init the model
-viewModel.variants[init_variant.variant] = init_variant;
-viewModel.load(init_variant.variant);
-ko.applyBindings(viewModel);
+}());
