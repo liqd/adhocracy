4ae80d09829063e83542e9e0e9cfabf3efa10c87
Carsten Senger <senger@rehfisch.de>
2011-12-06 13:58:57 +0100

    badges: add basic version of new badge style
    * Remove old css from styles.css
    * Add new css
    * Add background image
    * Add tile to generate a <script> block with rules
      for specific classes like badge_3 and render it in the
      page head
    * Change badge tiles to use the new markup
    * Change translation to generate better titles/tooltips

--- adhocracy/lib/tiles/badge_tiles.py
+++ adhocracy/lib/tiles/badge_tiles.py
@@ -1,9 +1,23 @@
 def badge(badge):
     from adhocracy.lib.templating import render_def
     return render_def('/badge/tiles.html', 'badge', badge=badge,
-                      cached=True)     
+                      cached=True)
+
 
 def badges(badges):
     from adhocracy.lib.templating import render_def
     return render_def('/badge/tiles.html', 'badges', badges=badges,
                       cached=True)
+
+
+def badge_styles():
+    '''
+    Render a <style>-block with dyamic badge styles
+    '''
+    from adhocracy.lib.templating import render_def
+    from adhocracy.model import Badge
+    badges = Badge.all()
+    r = render_def('/badge/tiles.html', 'badge_styles', badges=badges,
+                      cached=True)
+    print r
+    return r
