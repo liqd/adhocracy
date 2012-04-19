39d62f34dfc0f455e730fdf741462422c29377b4
Carsten Senger <senger@rehfisch.de>
2012-04-19 12:11:23 +0200

    Add option "adhocracy.debug.sql" to explain queries ttw
    * Add a config option adhocracy.debug.sql. If enabled the list of sql
      queries with timings will be displayed at the end of each page (that
      uses the root.html template).
    
    * For select queries it contains an 'EXPLAIN' link to a page with the
      query plan and the results table.

--- adhocracy/config/environment.py
+++ adhocracy/config/environment.py
@@ -1,10 +1,14 @@
 """Pylons environment configuration"""
 import os
+import time
+import traceback
 
 from mako.lookup import TemplateLookup
-from pylons import config
+from paste.deploy.converters import asbool
+from pylons import config, tmpl_context as c
 from pylons.error import handle_mako_error
 from sqlalchemy import engine_from_config
+from sqlalchemy.interfaces import ConnectionProxy
 
 import adhocracy.lib.app_globals as app_globals
 import adhocracy.lib.helpers
@@ -42,7 +46,10 @@ def load_environment(global_conf, app_conf, with_db=True):
         imports=['from webhelpers.html import escape'])
 
     # Setup the SQLAlchemy database engine
-    engine = engine_from_config(config, 'sqlalchemy.')
+    connectionproxy = None
+    if asbool(config.get('adhocracy.debug.sql', False)):
+        connectionproxy = TimerProxy()
+    engine = engine_from_config(config, 'sqlalchemy.', proxy=connectionproxy)
     init_model(engine)
 
     # CONFIGURATION OPTIONS HERE (note: all config options will override
@@ -51,3 +58,55 @@ def load_environment(global_conf, app_conf, with_db=True):
     if with_db:
         init_search()
     init_democracy()
+
+
+class TimerProxy(ConnectionProxy):
+    '''
+    A timing proxy with code borrowed from spline and
+    pyramid_debugtoolbar. This will work for sqlalchemy 0.6,
+    but not 0.7. pyramid_debugtoolbar works for 0.7.
+    '''
+
+    def cursor_execute(self, execute, cursor, statement, parameters, context,
+                       executemany):
+        start_time = time.time()
+
+        try:
+            return execute(cursor, statement, parameters, context)
+        finally:
+            duration = time.time() - start_time
+
+            # Find who spawned this query.  Rewind up the stack until we
+            # escape from sqlalchemy code -- including this file, which
+            # contains proxy stuff
+            caller = '(unknown)'
+            for frame_file, frame_line, frame_func, frame_code in \
+                reversed(traceback.extract_stack()):
+
+                if __file__.startswith(frame_file) \
+                    or '/sqlalchemy/' in frame_file:
+
+                    continue
+
+                # OK, this is it
+                caller = "{0}:{1} in {2}".format(
+                    frame_file, frame_line, frame_func)
+                break
+
+            # save interesting information for presentation later
+            try:
+                if not c.pdtb_sqla_queries:
+                    c.pdtb_sqla_queries = []
+                queries = c.pdtb_sqla_queries
+                query_data = {
+                    'duration': duration,
+                    'statement': statement,
+                    'parameters': parameters,
+                    'context': context,
+                    'caller': caller,
+                }
+                queries.append(query_data)
+            except TypeError:
+                # happens when sql is emitted before pylons has started
+                # or outside of a request
+                pass
