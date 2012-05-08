2d67961a4e125df50f57b285e09f21bffc1dd526
Carsten Senger <senger@rehfisch.de>
2012-05-08 12:23:54 +0200

    test/testbrowser: find doctests (use_cases/*.rst)

--- adhocracy/tests/test_doctest_files.py
+++ adhocracy/tests/test_doctest_files.py
@@ -1,9 +1,20 @@
+from glob import glob
 import doctest
 from doctest import DocFileSuite
+from os.path import dirname
 import unittest
+
 from adhocracy.tests.testbrowser import ADHOCRACY_LAYER
 from adhocracy.tests.testbrowser import ADHOCRACY_LAYER_APP
 from adhocracy.tests.testbrowser import Browser
+
+
+def find_use_cases():
+    here = dirname(__file__)
+    paths = glob('{here}/use_cases/*.rst'.format(here=here))
+    # we need relative paths for DocFileSuite
+    pathes = [path.replace(here, '.') for path in paths]
+    return pathes
 
 
 flags = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
@@ -11,6 +22,7 @@ globs = {"browser": Browser(wsgi_app=ADHOCRACY_LAYER_APP),
          "app":  ADHOCRACY_LAYER_APP,
          "app_url": "http://localhost",
         }
+use_cases = find_use_cases()
 
 
 class DoctestTestCase(unittest.TestCase):
@@ -21,7 +33,7 @@ class DoctestTestCase(unittest.TestCase):
     @classmethod
     def test_suite(self):
         return DocFileSuite(
-            "use_cases/test.rst",
+            *use_cases,
             #add here aditional testfiles
             setUp=ADHOCRACY_LAYER.setUp,
             tearDown=ADHOCRACY_LAYER.tearDown,
