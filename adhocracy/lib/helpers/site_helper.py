0c5f0d1b631e4b9fd7aace5179f262f25107d53a
Philipp Hagemeister <phihag@phihag.de>
2012-06-01 15:45:08 +0200

    Option to use relative instead of absolute URLs

--- adhocracy/lib/helpers/site_helper.py
+++ adhocracy/lib/helpers/site_helper.py
@@ -1,6 +1,6 @@
 from pylons import config, g
 from pylons.i18n import _
-
+from paste.deploy.converters import asbool
 
 def domain():
     return config.get('adhocracy.domain').split(':')[0]
@@ -11,11 +11,18 @@ def name():
 
 
 def base_url(instance, path=None):
-    url = "%s://" % config.get('adhocracy.protocol', 'http').strip()
-    if instance is not None and g.single_instance is None:
-        url += instance.key + "."
-    url += config.get('adhocracy.domain').strip()
-    if path is not None:
+    if asbool(config.get('adhocracy.urls_without_hostname', 'false')):
+        url = ''
+        if instance is not None:
+            url = '/i/' + instance.key
+    else:
+        url = "%s://" % config.get('adhocracy.protocol', 'http').strip()
+        if instance is not None and g.single_instance is None:
+            url += instance.key + "."
+        url += config.get('adhocracy.domain').strip()
+    if path is None:
+        url += '/'
+    else:
         url += path
     return url
 
