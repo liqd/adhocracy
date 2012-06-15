43e1641e8efb1d05430564a3aa8e7a637d06118e
Philipp Hagemeister <phihag@phihag.de>
2012-06-15 14:03:52 +0200

    Helper method for absolute URLs

--- adhocracy/lib/helpers/site_helper.py
+++ adhocracy/lib/helpers/site_helper.py
@@ -9,23 +9,23 @@ def domain():
 def name():
     return config.get('adhocracy.site.name', _("Adhocracy"))
 
+def absolute_url(instance, path=None):
+    url = "%s://" % config.get('adhocracy.protocol', 'http').strip()
+    if instance is not None and g.single_instance is None:
+        url += instance.key + "."
+    url += config.get('adhocracy.domain').strip()
+    url += '/' if path is None else path
+    return url
 
 def base_url(instance, path=None):
     if asbool(config.get('adhocracy.urls_without_hostname', 'false')):
         url = ''
         if instance is not None:
             url = '/i/' + instance.key
+        url += '/' if path is None else path
+        return url
     else:
-        url = "%s://" % config.get('adhocracy.protocol', 'http').strip()
-        if instance is not None and g.single_instance is None:
-            url += instance.key + "."
-        url += config.get('adhocracy.domain').strip()
-    if path is None:
-        url += '/'
-    else:
-        url += path
-    return url
-
+        return absolute_url(instance, path)
 
 def shortlink_url(delegateable):
     path = "/d/%s" % delegateable.id
