141f0133fdf8ede7cced1b76652c8ff2676ec13b
Nicolas Dietrich <nicodietrich@gmail.com>
2012-07-25 18:14:28 +0200

    adjust automatic instance creation
     - make nicer instance keys, which are valid domain elements (no underscore)
     - add stadtstaaten to created instances
     - minor stuff

--- scripts/create-regional-instances.py
+++ scripts/create-regional-instances.py
@@ -4,6 +4,8 @@
 Create instances from selected regions. Currently hardcoded for
 offenekommune.de purposes.
 
+Run this script after creating the region hierarchy.
+
 call it with:
 LD_LIBRARY_PATH=parts/geos/lib bin/adhocpy src/adhocracy/scripts/create-regional-instances.py etc/adhocracy.ini
 """
@@ -11,38 +13,29 @@ LD_LIBRARY_PATH=parts/geos/lib bin/adhocpy src/adhocracy/scripts/create-regional
 # boilerplate code. copy that
 import os
 import sys
-sys.path.insert(0,  os.path.abspath(os.path.dirname(__file__)))
-sys.path.insert(0,  os.path.join(os.path.abspath(os.path.dirname(__file__)), 'src/adhoracy'))
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, os.path.join(
+    os.path.abspath(os.path.dirname(__file__)),
+    'src/adhoracy'))
 from common import create_parser, load_from_args
 # /end boilerplate code
 
+from sqlalchemy import not_
+
+from adhocracy.lib.geo import normalize_region_name
 from adhocracy.model import meta
 from adhocracy.model import Instance
 from adhocracy.model import Region
 from adhocracy.model import User
+from adhocracy.model.instance import instance_table
 
 
-replacements = {
-    u'ä': u'ae',
-    u'ö': u'oe',
-    u'ü': u'ue',
-    u'Ä': u'Ae',
-    u'Ö': u'Oe',
-    u'Ü': u'Ue',
-    u'ß': u'ss',
-    u' ': u'_',
-    u'.': u'_',
-    u'(': u'',
-    u')': u'',
-    }
-
-
-# current setting in instance
-MAX_KEY_LENGTH = 20
+MAX_KEY_LENGTH = instance_table.columns.key.type.length
 
 
 def removePrefix(str, prefix):
     return str[len(prefix):] if str.startswith(prefix) else str
+
 
 def normalize_label(s):
 
@@ -57,14 +50,20 @@ def normalize_label(s):
 
     return s
 
+
 def normalize_key(s):
-    """ make region names a sensitive and nice subdomain url """
-    return reduce(lambda a, kv: a.replace(*kv), replacements.iteritems(),
-            s.lower())
+    """ normalize_key creates a sensitive and nice subdomain url """
+    key = normalize_region_name(s)
+
+    if len(key) > MAX_KEY_LENGTH:
+        print("stripping key %s with %d characters" % (key, len(key)))
+        key = key[:MAX_KEY_LENGTH]
+        key.rstrip(u'-')
+
+    return key
+
 
 def create_municipality(region):
-
-    print("creating %s"%region.name)
 
     label = region.name
 
@@ -75,17 +74,20 @@ def create_municipality(region):
     if label == u'Oldenburg' and region.id == 62409:
         label = u'Stadt Oldenburg'
 
+    if label == u'Freie Hansestadt Bremen':
+        label = u'Bremen'
+
     label = normalize_label(label)
 
-    key = normalize_key(label)[:MAX_KEY_LENGTH]
+    key = normalize_key(label)
 
-    if meta.Session.query(Instance).filter(Instance.key==key).count()>0:
-        print("there is already an instance with key %s"%key)
+    if meta.Session.query(Instance).filter(Instance.key == key).count() > 0:
+        print("there is already an instance with key %s" % key)
         return
 
-    description = "Willkommen bei Kommune %s"%label
+    description = "Willkommen bei Kommune %s" % label
 
-    user = meta.Session.query(User).filter(User.user_name==u'admin').one()
+    user = meta.Session.query(User).filter(User.user_name == u'admin').one()
 
     locale = 'de_DE'
 
@@ -106,7 +108,7 @@ def create_municipality(region):
     instance.milestones = False
     instance.allow_adopt = False
 
-    print("instance %s created"%key)
+    print("instance %s created" % key)
 
 
 def main():
@@ -114,12 +116,23 @@ def main():
     args = parser.parse_args()
     load_from_args(args)
 
-    for region in meta.Session.query(Region).filter(Region.admin_level==6):
+    stadtstaaten = (u'Berlin', u'Freie Hansestadt Bremen', u'Hamburg')
+
+    stadtstaaten_q = meta.Session.query(Region).filter(Region.admin_level == 4)\
+        .filter(Region.name.in_(stadtstaaten))
+
+    a6_query = meta.Session.query(Region).filter(Region.admin_level == 6)\
+        .join(Region.outer_regions, aliased=True)\
+        .filter(Region.admin_level == 4)\
+        .filter(not_(Region.name.in_(stadtstaaten)))
+
+    for region in a6_query.union(stadtstaaten_q).all():
 
         try:
             create_municipality(region)
         except Exception, e:
-            print("couldn't create instance for region %s, because of %s"%(region.name,e))
+            print("couldn't create instance for region %s, because of %s" %
+                  (region.name, e))
 
     meta.Session.commit()
 
