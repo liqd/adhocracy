4d203264776f24510f6f9f0d2f39b427e0b604ae
Philipp Hagemeister <phihag@phihag.de>
2012-08-08 20:04:49 +0200

    Don't use quotes when setting cookies, and assert that the cookie value conforms to the RFC

--- adhocracy/lib/auth/instance_auth_tkt.py
+++ adhocracy/lib/auth/instance_auth_tkt.py
@@ -1,10 +1,14 @@
 import datetime
+import re
 from repoze.who.plugins.auth_tkt import AuthTktCookiePlugin, _now
 
+# Valid cookie values, see http://tools.ietf.org/html/rfc6265#section-4.1.1
+_COOKIE_VALUE_RE = re.compile(u'^[!#$%&\'()*+./0-9:<=>?@A-Z[\\]^_`a-z{|}~-]*$')
 
 class InstanceAuthTktCookiePlugin(AuthTktCookiePlugin):
 
     def _get_cookies(self, environ, value, max_age=None):
+        assert _COOKIE_VALUE_RE.match(value)
         if max_age is not None:
             later = _now() + datetime.timedelta(seconds=int(max_age))
             # Wdy, DD-Mon-YY HH:MM:SS GMT
@@ -17,12 +21,9 @@ class InstanceAuthTktCookiePlugin(AuthTktCookiePlugin):
 
         cur_domain = environ.get('adhocracy.domain').split(':')[0]
         wild_domain = '.' + cur_domain
+
         cookies = [
-            #('Set-Cookie', '%s="%s"; Path=/%s' % (
-            #self.cookie_name, value, max_age)),
-            #('Set-Cookie', '%s="%s"; Path=/; Domain=%s%s' % (
-            #self.cookie_name, value, cur_domain, max_age)),
-            ('Set-Cookie', '%s="%s"; Path=/; Domain=%s%s' % (
+            ('Set-Cookie', '%s=%s; Path=/; Domain=%s%s' % (
             self.cookie_name, value, wild_domain, max_age))
             ]
         return cookies
