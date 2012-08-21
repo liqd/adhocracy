545348425ec29475d507822e02f1c79d266c69fa
Christopher Probst <christopher.probst@uni-duesseldorf.de>
2012-08-21 13:46:54 +0200

    Machine_name middleware moved and renamed

--- adhocracy/lib/machine_name.py
+++ adhocracy/lib/machine_name.py
@@ -2,18 +2,14 @@
 import platform
 from webob import Request, Response
 
-class InterceptorMiddleware(object):
+class IncludeMachineName(object):
     
     def __init__(self, app, config):
         self.app = app
         self.config = config
 
     def __call__(self, environ, start_response):
-        #def local_response(status, headers):
-        #    headers += [('X-Server-Machine', platform.node())]
-        #    start_response(status, headers)
         req = Request(environ)
         rsp = req.get_response(self.app)
         rsp.headers['X-Server-Machine'] = platform.node()
         return rsp(environ, start_response)
-        #return self.app(environ, start_response)
