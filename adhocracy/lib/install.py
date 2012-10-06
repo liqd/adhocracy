liqd/adhocracy

commit aea47d358620de9d9a64b73e48868db67e1435de

Add role moderator; observer can view and watch
* Add a new role 'moderator' that is similar to an instance
  administrator ('supervisor') without permissions to administrate the
  instance itself.

* Change permissions for observer. it now inherits from anonymous and
  has beside it's permission to view only the permission to manage
  (his own) watches.

adhocracy/lib/install.py
@@ -34,9 +34,12 @@ def setup_entities():
     #model.meta.Session.begin()
     model.meta.Session.commit()
 
+    # administrate installation wide
     admins = mk_group("Administrator", model.Group.CODE_ADMIN)
     organization = mk_group("Organization", model.Group.CODE_ORGANIZATION)
+    # administrate instance
     supervisor = mk_group("Supervisor", model.Group.CODE_SUPERVISOR)
+    moderator = mk_group("Moderator", model.Group.CODE_MODERATOR)
     voter = mk_group("Voter", model.Group.CODE_VOTER)
     observer = mk_group("Observer", model.Group.CODE_OBSERVER)
     advisor = mk_group("Advisor", model.Group.CODE_ADVISOR)
@@ -60,15 +63,15 @@ def setup_entities():
     mk_perm("comment.show", anonymous)
     mk_perm("comment.create", advisor)
     mk_perm("comment.edit", advisor)
-    mk_perm("comment.delete", supervisor)
+    mk_perm("comment.delete", moderator)
     mk_perm("proposal.create", advisor)
     mk_perm("proposal.edit", advisor)
-    mk_perm("proposal.delete", supervisor)
+    mk_perm("proposal.delete", moderator)
     mk_perm("proposal.view", anonymous)
     mk_perm("proposal.show", anonymous)
     mk_perm("poll.show", anonymous)
-    mk_perm("poll.create", supervisor)
-    mk_perm("poll.delete", supervisor)
+    mk_perm("poll.create", moderator)
+    mk_perm("poll.delete", moderator)
     mk_perm("user.manage", admins)
     mk_perm("user.edit", default)
     mk_perm("user.view", anonymous)
@@ -79,8 +82,8 @@ def setup_entities():
     mk_perm("delegation.create", voter)
     mk_perm("delegation.delete", voter)
     mk_perm("watch.show", anonymous)
-    mk_perm("watch.create", advisor)
-    mk_perm("watch.delete", advisor)
+    mk_perm("watch.create", observer)
+    mk_perm("watch.delete", observer)
     mk_perm("tag.show", anonymous)
     mk_perm("tag.view", anonymous)
     mk_perm("tag.create", advisor)
@@ -89,7 +92,7 @@ def setup_entities():
     mk_perm("page.view", anonymous)
     mk_perm("page.create", advisor)
     mk_perm("page.edit", advisor)
-    mk_perm("page.delete", supervisor)
+    mk_perm("page.delete", moderator)
     mk_perm("milestone.show", anonymous)
     mk_perm("milestone.create", supervisor)
     mk_perm("milestone.edit", supervisor)
@@ -101,10 +104,11 @@ def setup_entities():
     model.meta.Session.commit()
     # END PERMISSIONS LIST
 
-    advisor.permissions = advisor.permissions + anonymous.permissions
-    observer.permissions = observer.permissions + advisor.permissions
+    observer.permissions = observer.permissions + anonymous.permissions
+    advisor.permissions = advisor.permissions + observer.permissions
     voter.permissions = voter.permissions + observer.permissions
-    supervisor.permissions = supervisor.permissions + voter.permissions
+    moderator.permissions = moderator.permissions + voter.permissions
+    supervisor.permissions = supervisor.permissions + moderator.permissions
     admins.permissions = admins.permissions + supervisor.permissions
     organization.permissions = organization.permissions + observer.permissions
 

commit 5eec86bc5ccc57a1100ae09c3382c7f5fc9c00bb

Fix #71 (Sensible default permissions)
Fixes wrong permissions for default role `voter`, which has been introduced in
commit aea47d358620de9d9a64b73e48868db67e1435de.

adhocracy/lib/install.py
@@ -107,7 +107,7 @@ def setup_entities():
 
     observer.permissions = observer.permissions + anonymous.permissions
     advisor.permissions = advisor.permissions + observer.permissions
-    voter.permissions = voter.permissions + observer.permissions
+    voter.permissions = voter.permissions + advisor.permissions
     moderator.permissions = moderator.permissions + voter.permissions
     supervisor.permissions = list(set(supervisor.permissions
                                + moderator.permissions + advisor.permissions))