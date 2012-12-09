60ddfc57f9fd1a8219b76150ca56f1d0d0dff0d2
Nicolas Dietrich <nicodietrich@gmail.com>
2012-12-09 23:45:16 +0100

    Replace armlabel against html5 placeholders
    Following commit 9ee43dc9c6825cf6671a15213914d6c22a9d6c5a (replace
    jquery.label_over with html5 placeholders), the javascript based
    armlabel placeholder mechanism is now also replaced by proper html5
    placeholders, compatibility assured via the jquery-placeholder polyfill
    library.

--- adhocracy/static/javascripts/adhocracy.js
+++ adhocracy/static/javascripts/adhocracy.js
@@ -489,34 +489,6 @@ $(document).ready(function () {
     );
 
 
-    /* Armed labels: Use label text as pre-filling text for empty form fields. */
-    $(".armlabel").each(function (e) {
-        var hint = $("[for=" + $(this).attr("name") + "]").text();
-        var field = this;
-
-        $(this).focus(function () {
-            if ($(field).hasClass("armed")) {
-                $(field).val("");
-                $(field).removeClass("armed");
-            }
-        });
-
-        $(this).blur(function () {
-            if ($.trim($(field).val()).length === 0) {
-                $(field).val(hint);
-                $(field).addClass("armed");
-            }
-        });
-        $(this).blur();
-    });
-
-    /* Make sure that we do not submit placeholder texts */
-    $("form").submit(function () {
-        $(".armed").each(function (i) {
-            $(this).val("");
-        });
-    });
-
     /* Hide hidejs class elements, e.g. input field in user.register */
     $(".hidejs").hide();
 
