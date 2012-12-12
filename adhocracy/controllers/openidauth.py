92a8009f4b1791789d4b7c443892eee10e6d8d56
Nicolas Dietrich <nicodietrich@gmail.com>
2012-12-12 18:05:58 +0100

    Explicitly pass response to login_user (fixes OpenID login)
    Request.response doesn't exist anymore after Pylons/WebOb upgrade.

--- adhocracy/controllers/openidauth.py
+++ adhocracy/controllers/openidauth.py
@@ -57,7 +57,7 @@ class OpenidauthController(BaseController):
         """
         log the user in and redirect him to a sane place.
         """
-        login_user(user, request)
+        login_user(user, request, response)
         if c.instance and not user.is_member(c.instance):
             redirect(h.base_url("/instance/join/%s?%s" % (c.instance.key,
                                                     h.url_token())))
