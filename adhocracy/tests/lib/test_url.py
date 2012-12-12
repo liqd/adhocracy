d477298247b3d4a77462dc222bc58a0871e6e891
Philipp Hagemeister <phihag@phihag.de>
2012-12-12 03:02:26 +0100

    Merge test_url from develop

--- adhocracy/tests/lib/test_url.py
+++ adhocracy/tests/lib/test_url.py
@@ -34,52 +34,58 @@ class TestUrls(TestController):
         from adhocracy.lib.helpers.url import build
         url = build(None, 'base', 'id', query={'param': 'arg'},
                     anchor='anchor', member='member', format="html")
-        self.assertTrue(u'/base/id/member.html#anchor?param=arg' in url)
+        self.assertEqual(
+            url, u'/base/id/member.html#anchor?param=arg')
 
     def test_build_global_omit_base(self):
         from adhocracy.lib.helpers.url import build
         url = build(None, None, 'id', query={'param': 'arg'},
                     anchor='anchor', member='member', format="html")
-        self.assertTrue(u'/id/member.html#anchor?param=arg' in url)
+        self.assertEqual(url,
+                         u'/id/member.html#anchor?param=arg')
         url = build(None, '', 'id', query={'param': 'arg'},
                     anchor='anchor', member='member', format="html")
-        self.assertTrue(u'/id/member.html#anchor?param=arg' in url)
+        self.assertEqual(url,
+                         u'/id/member.html#anchor?param=arg')
 
     def test_build_global_omit_member_and_format(self):
         from adhocracy.lib.helpers.url import build
         url = build(None, 'base', 'id', query={'param': 'arg'},
                     anchor='anchor')
-        self.assertTrue(u'/base/id#anchor?param=arg' in url)
+        self.assertEqual(url, u'/base/id#anchor?param=arg')
 
     def test_build_global_omit_anchor_member_and_format(self):
         from adhocracy.lib.helpers.url import build
         url = build(None, 'base', 'id', query={'param': 'arg'},
                     anchor='anchor')
-        self.assertTrue(u'/base/id#anchor?param=arg' in url)
+        self.assertEqual(url, u'/base/id#anchor?param=arg')
 
     def test_build_global_omit_query(self):
         from adhocracy.lib.helpers.url import build
         url = build(None, 'base', 'id', anchor='anchor', member='member',
                     format="html")
-        self.assertTrue(u'/base/id/member.html#anchor' in url)
+        self.assertEqual(url,
+                         u'/base/id/member.html#anchor')
 
     def test_build_query_is_encoded(self):
         from adhocracy.lib.helpers.url import build
         url = build(None, 'base', 'id', query={'param': 'http://a@b:x'})
-        self.assertTrue(u'base/id?param=http%3A%2F%2Fa%40b%3Ax' in url)
+        self.assertEqual(
+            url, u'/base/id?param=http%3A%2F%2Fa%40b%3Ax')
 
     def test_build_global_omit_all_except_id(self):
         from adhocracy.lib.helpers.url import build
         url = build(None, None, 'id')
-        self.assertTrue(u'/id' in url)
+        self.assertEqual(url, u'/id')
 
     def test_login_redirect(self):
         from adhocracy.lib.helpers import login_redirect_url
 
         proposal = tt_make_proposal(title='testproposal')
         url = login_redirect_url(proposal)
-        self.assertTrue(u'/login?came_from=' in url)
-        self.assertTrue(url.endswith(u'%2Fproposal%2F2-testproposal'))
+        expected = (u'/login?came_from='
+                    u'%2Fproposal%2F2-testproposal')
+        self.assertEqual(url, expected)
 
 
 class TestInstanceUrls(TestController):
@@ -93,7 +99,8 @@ class TestInstanceUrls(TestController):
             from adhocracy.tests.testtools import tt_get_instance
             test_instance = tt_get_instance()
             url = h.instance.icon_url(test_instance, 48)
-            self.assertTrue('/instance/test_48.png?t=1234' in url)
+            self.assertEqual(
+                url, '/instance/test_48.png?t=1234')
 
     def test_icon_url_with_x_and_y(self):
         with self.mocked_path_func:
@@ -101,7 +108,8 @@ class TestInstanceUrls(TestController):
             from adhocracy.tests.testtools import tt_get_instance
             test_instance = tt_get_instance()
             url = h.instance.icon_url(test_instance, 48, x=11)
-            self.assertTrue('/instance/test_11x48.png?t=1234' in url)
+            self.assertEqual(
+                url, '/instance/test_11x48.png?t=1234')
 
     def test_icon_url_contains_mtime(self):
         with self.mocked_path_func:
@@ -109,10 +117,5 @@ class TestInstanceUrls(TestController):
             from adhocracy.tests.testtools import tt_get_instance
             test_instance = tt_get_instance()
             url = h.instance.icon_url(test_instance, 48)
-            self.assertTrue('/instance/test_48.png?t=1234' in url)
-
-class TestBaseUrl(TestController):
-    def test_base_url_absolute(self):
-        from adhocracy.lib import base_url
-        self.assertTrue(u'/' in base_url())
-        self.assertTrue(base_url(absolute=True).startswith(u'http'))
+            self.assertEqual(
+                url, '/instance/test_48.png?t=1234')
