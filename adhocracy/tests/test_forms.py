8bdcd985a5fc816d7032a1edd2828b317eb8522b
Nicolas Dietrich <nicodietrich@gmail.com>
2012-12-12 02:19:46 +0100

    Fix tests after addition of invisible badges
    Addendum to 48ec5f6daa3c7e489d9f023ee68141ac1f9d9ccf

--- adhocracy/tests/test_forms.py
+++ adhocracy/tests/test_forms.py
@@ -8,7 +8,7 @@ class TestValidators(TestController):
         from adhocracy.forms import ValidUserBadge
         from adhocracy.model import UserBadge
 
-        badge = UserBadge.create('testbadge', '#ccc', 'description')
+        badge = UserBadge.create('testbadge', '#ccc', True, 'description')
         value = ValidUserBadge.to_python(badge.id, None)
         self.assertEqual(value, badge)
 
@@ -17,7 +17,7 @@ class TestValidators(TestController):
         from adhocracy.forms import ValidUserBadge
         from adhocracy.model import UserBadge
 
-        badge = UserBadge.create('testbadge', '#ccc', 'description')
+        badge = UserBadge.create('testbadge', '#ccc', True, 'description')
         self.assertRaises(Invalid, ValidUserBadge.to_python,
                           badge.id + 1, state=None)
 
@@ -41,14 +41,14 @@ class TestValidators(TestController):
         # the current instance are valid.
         test_instance = tt_get_instance()
         self.assertEqual(test_instance, instance_filter.get_instance())
-        test_category = CategoryBadge.create('test_category', '#ccc',
+        test_category = CategoryBadge.create('test_category', '#ccc', True,
                                              'description', test_instance)
         value = ValidCategoryBadge.to_python(str(test_category.id))
         self.assertEqual(value, test_category)
 
         # from other instances they are not valid
         other_instance = tt_make_instance('other', 'Other Instance')
-        other_category = CategoryBadge.create('other_category', '#ccc',
+        other_category = CategoryBadge.create('other_category', '#ccc', True,
                                               'description', other_instance)
         self.assertRaises(Invalid, ValidCategoryBadge.to_python,
                           str(other_category.id))
