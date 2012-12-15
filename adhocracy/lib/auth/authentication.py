693ded6fd66eec82a419a6b9ebe3e7d05d5cd1fd
Philipp Hagemeister <phihag@phihag.de>
2012-12-15 19:09:27 +0100

    Explicitly store adhocracy config in InstanceAuthTktCookiePlugin (Fixes #168)
    Since the pylons upgrade (db63816f1f012d231a838077a314d55e6ba54114), pylons.config only contains Pylon's internal configuration at this place. Explicitly storing the configuration solves this.

--- adhocracy/lib/auth/authentication.py
+++ adhocracy/lib/auth/authentication.py
@@ -28,6 +28,7 @@ def setup_auth(app, config):
 
     basicauth = BasicAuthPlugin('Adhocracy HTTP Authentication')
     auth_tkt = InstanceAuthTktCookiePlugin(
+        config,
         '41d207498d3812741e27c6441760ae494a4f9fbf',
         cookie_name='adhocracy_login', timeout=86400 * 2,
         reissue_time=3600)
