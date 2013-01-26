2e0324706ede73df9483ace2a79b878a8b3affe4
Drilon Aziri <mail@drilon.info>
2013-01-26 17:24:49 +0100

    new order of migrationscripts #197

---
+++ adhocracy/migration/versions/048_add_request_log_table.py
@@ -0,0 +1,20 @@
+from sqlalchemy import MetaData, Column, ForeignKey, Table
+from sqlalchemy import Boolean, DateTime, Integer, Unicode, String, UnicodeText, Enum
+
+meta = MetaData()
+
+request_table = Table('request', meta,
+    Column('id', Integer, primary_key=True),
+    Column('cookies', UnicodeText()),
+    Column('remote_ip_address', Unicode(255)),
+    Column('useragent', UnicodeText()),
+    Column('request_url', UnicodeText()),
+    Column('proxy', UnicodeText()),
+    )
+
+def upgrade(migrate_engine):
+    meta.bind = migrate_engine
+    request_table.create()
+
+def downgrade(migrate_engine):
+    raise NotImplementedError()
