aaed25be863c707165ca6a83e75eadaf8b772c25
Philipp Hagemeister <phihag@phihag.de>
2013-02-04 02:26:02 +0100

    Actual anonymization

--- adhocracy/lib/requestlog.py
+++ adhocracy/lib/requestlog.py
@@ -1,17 +1,29 @@
 import ipaddress
+import logging
 
 from adhocracy import model
 
 def _anonymize(ipstr):
-    ipa = ipaddress.ip_address(ipstr)
-    print(dir(ipa))
-    raise NotImplementedError()
+    ipa = ipaddress.ip_address(unicode(ipstr))
+    if ipa.version == 4:
+        keep_bits = 24
+    elif ipa.version == 6:
+        if ipa.ipv4_mapped:
+            keep_bits = 120
+        else:
+            keep_bits = 48
+    else:
+        raise NotImplementedError()
+    mask = ((1 << keep_bits) - 1) << (ipa.max_prefixlen - keep_bits)
+    return unicode(type(ipa)(int(ipa) & mask))
 
 ANONYMIZATION_FUNCS = {
-    'dontlog': lambda ip: None,
+    'dontlog': lambda ipstr: None,
     'anonymize': _anonymize,
-    'none': lambda ip: ip,
+    'none': lambda ipstr: unicode(ipstr),
 }
+
+log = logging.getLogger(__name__)
 
 class RequestLogger(object):
     def __init__(self, app, config):
@@ -23,8 +35,8 @@ class RequestLogger(object):
     def __call__(self, environ, start_response):
         try:
             self.log_request(environ)
-        except:
-            pass
+        except BaseException as e:
+            log.error('Error while trying to log request: %r' % e)
         return self.app(environ, local_response)
 
     def log_request(self, environ):
@@ -32,8 +44,8 @@ class RequestLogger(object):
         if do_logging:
             self.log_request(environ)
         
-        cookies = environ.get('HTTP_COOKIE')
-        user_agent = environ.get('HTTP_USER_AGENT')
+        cookies = environ.get('HTTP_COOKIE').decode('utf-8', 'replace')
+        user_agent = environ.get('HTTP_USER_AGENT').decode('utf-8', 'replace')
         ip_address = self.anonymization_func(adhocracy.lib.util.get_client_ip())
 
         model.Request.create(ip_adress, environ['PATH_INFO'], cookies, user_agent)
--- adhocracy/tests/lib/test_requestlog.py
+++ adhocracy/tests/lib/test_requestlog.py
@@ -9,15 +9,15 @@ class RequestLogTest(TestController):
         self.assertEqual(no_anonymization(u'127.8.3.2'), u'127.8.3.2')
 
         dontlog = adhocracy.lib.requestlog.ANONYMIZATION_FUNCS['dontlog']
-        self.assertEqual(dontlog('::1'), None)
+        self.assertEqual(dontlog(u'::1'), None)
         self.assertEqual(dontlog(u'127.8.3.2'), None)
 
     def test_anonymization(self):
         af = adhocracy.lib.requestlog.ANONYMIZATION_FUNCS['anonymize']
-        self.assertEqual(af('9.8.3.3'), '9.8.3.0')
-        self.assertEqual(af('12.234.122.254'), '12.234.122.0')
-        self.assertEqual(af('2001:6f8:1377:2a11:1:2:3:4567'), '2001:6f8:1377:2a11::')
-        self.assertEqual(af('2001:6f8:1377:2a11:1:2::4567'), '2001:6f8:1377:2a11::')
-        self.assertEqual(af('2001:6f8:1377:2a11::fe:1.2.3.4'), '2001:6f8:1377:2a11::')
-        self.assertEqual(af('2001::1377:2a11::fe:1.2.3.4'), '2001:0:1377:2a11::')
-        self.assertEqual(af('::ffff:12.234.122.254'), '12.234.122.0')
+        self.assertEqual(af(u'9.8.3.3'), u'9.8.3.0')
+        self.assertEqual(af(u'12.234.122.254'), u'12.234.122.0')
+        self.assertEqual(af(u'2001:6f8:1377:2a11:1:2:3:4567'), u'2001:6f8:1377::')
+        self.assertEqual(af(u'2001:6f8:1377:2a11:1:2::4567'), u'2001:6f8:1377::')
+        self.assertEqual(af(u'2001:6f8:1377:2a11::fe:1.2.3.4'), u'2001:6f8:1377::')
+        self.assertEqual(af(u'2001::1377:2a11:0:fe:1.2.3.4'), u'2001:0:1377::')
+        self.assertEqual(af(u'::ffff:12.234.122.254'), u'::ffff:cea:7a00')
