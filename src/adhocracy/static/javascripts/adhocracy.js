b3daa66dc915a618b2a658e9183aa112d30a23df
Nicolas Dietrich <nicodietrich@gmail.com>
2013-02-07 21:40:40 +0100

    Add checkboxes to facet items
    and style facet items differently.

--- src/adhocracy/static/javascripts/adhocracy.js
+++ src/adhocracy/static/javascripts/adhocracy.js
@@ -651,4 +651,8 @@ $(document).ready(function () {
     $('a.expand_arrow').click(function () {
         $(this).parent().toggleClass('expanded');
     });
+
+    $('.facet_check').click(function() {
+        $(this).parent().children('a')[0].click();
+    });
 });
