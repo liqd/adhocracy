1b461c3c339c24d180273a83c9bd56d895b00780
Philipp Hagemeister <phihag@phihag.de>
2013-03-04 17:31:24 +0100

    Re-add missing fields in migration scripts (Fixes hhucn/adhocracy.hhu_theme#162)

--- src/adhocracy/migration/versions/047_add_gender_column.py
+++ src/adhocracy/migration/versions/047_add_gender_column.py
@@ -23,6 +23,7 @@ user_table = Table('user', meta,
     Column('banned', Boolean, default=False),
     Column('no_help', Boolean, default=False, nullable=True),
     Column('page_size', Integer, default=10, nullable=True),
+    Column('proposal_sort_order', Unicode(50), default=None, nullable=True),
     )
 
 def upgrade(migrate_engine):
