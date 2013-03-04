304f595472df42cea90448374f751d1992c67bc4
Nicolas Dietrich <nicodietrich@gmail.com>
2013-03-04 05:06:32 +0100

    Fix missing import
    This got broken during the latest large merge.

--- src/adhocracy/controllers/comment.py
+++ src/adhocracy/controllers/comment.py
@@ -11,7 +11,7 @@ from pylons.i18n import _
 from adhocracy import forms, model
 from adhocracy.lib import democracy
 from adhocracy.lib import event, helpers as h, sorting, tiles, watchlist
-from adhocracy.lib.auth import can, csrf, require
+from adhocracy.lib.auth import can, csrf, require, guard
 from adhocracy.lib.auth.authorization import has_permission
 from adhocracy.lib.base import BaseController
 from adhocracy.lib.instance import RequireInstance
