b9be95412843c8f98a0c2cc6b67913dbbc1aefba
Matthias Radig <radig@cs.uni-duesseldorf.de>
2013-03-07 16:56:39 +0100

    add documentation to stats controller

--- src/adhocracy/controllers/stats.py
+++ src/adhocracy/controllers/stats.py
@@ -2,6 +2,8 @@ from adhocracy.lib.base import BaseController
 from adhocracy.lib.templating import render_json
 
 class StatsController(BaseController):
-
+    """ This controller is an endpoint for AJAX requests that monitor user
+    behavior. You'll want to enable `adhocracy.requestlog_active` to record
+    them.  """
     def read_comments(self, format='json'):
         return render_json({})
