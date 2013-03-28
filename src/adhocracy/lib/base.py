d961924cf6123152b6b16d1bf4bf1964ebae21fd
Matthias Radig <radig@cs.uni-duesseldorf.de>
2013-03-28 15:37:13 +0100

    Fix page monitoring to include full request URL
    The monitoring process for the time spent on a page logs only a partial
    URL. This is fixed by using location.href when submitting the background
    request.

--- src/adhocracy/lib/base.py
+++ src/adhocracy/lib/base.py
@@ -48,9 +48,7 @@ class BaseController(WSGIController):
                 config.get('adhocracy.monitor_page_time_interval', -1))
         if monitor_page_time_interval > 0:
             c.monitor_page_time_interval = monitor_page_time_interval
-            c.monitor_page_time_url = '%s?%s' % (
-                h.base_url('/stats/on_page'),
-                urllib.urlencode({'path': request.url}))
+            c.monitor_page_time_url = h.base_url('/stats/on_page')
 
         h.add_rss("%s News" % h.site.name(),
                   h.base_url('/feed.rss', None))
