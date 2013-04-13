f95d76c5d879fb717ed3fe6490b18ff38215001e
Joscha Krutzki <joka@jokasis.de>
2013-04-13 14:05:16 +0200

    adhocracy_kotti: set available image scales

--- src/adhocracy_kotti/adhocracy_kotti/tests/test_configure.py
+++ src/adhocracy_kotti/adhocracy_kotti/tests/test_configure.py
@@ -1,6 +1,13 @@
 # test kotti configuration
 
 
-def test_populate(db_session):  # pytest public fixture: kotti.tests.db_session
-    from kotti.resources import Document
-    import transaction
+def test_configure_image_scales(config):
+    from adhocracy_kotti import kotti_configure
+    from adhocracy_kotti import schemata
+    from kotti.util import extract_from_settings
+    settings = config.registry.settings
+    kotti_configure(settings)
+    available_scales = extract_from_settings('kotti.image_scales.',
+                                             settings).keys().sort()
+    needed_scales = schemata.ImageScale.validator.choices.sort()
+    assert available_scales == needed_scales
