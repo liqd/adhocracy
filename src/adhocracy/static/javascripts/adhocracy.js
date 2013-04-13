d06b0c812c0c28170fb5aac2d40b818e70ee71a5
Richard Liebig <liebig.richard@hotmail.com>
2013-04-13 15:48:17 +0200

    Code update

--- src/adhocracy/static/javascripts/adhocracy.js
+++ src/adhocracy/static/javascripts/adhocracy.js
@@ -498,32 +498,37 @@ $(document).ready(function () {
     if (page_stats_baseurl) {
 
         var stats_extended = $('body').data('stats-extended');
-        if (stats_extended != undefined) {
+        if (stats_extended) {
             var start_time = new Date();
             var page_stats_data = new Array();
 
-            var add_to_page_stats = function(type, data="") {
+            var add_to_page_stats = function(type, data) {
                 var timestamp = new Date() - start_time;
-                page_stats_data.push({"time": timestamp, "type": type,
-                    "data": data});
+                var event = {"time": timestamp, "type": type};
+                if(data) {
+                    event.data = data;
+                }
+                page_stats_data.push(event);
             };
         
             var get_path = function(element) {
                 return $(element).parentsUntil('body').andSelf().map(function() {
-                    if (this.id == undefined) {
-                        return this.nodeName + '[' + $(this).index() + ']'; 
-                    } else {
+                    if (this.id) {
                         return this.nodeName + '#' + this.id;
+                    } else {
+                        return this.nodeName + '[' + $(this).index() + ']'; 
                     }
                 }).get().join('>');
             };
 
             $(document).on("keydown", function(e) {
+                // Anonymize [a-Z][0-9] to prevent recording confidential data
                 if ((e.keyCode >= 65 && e.keyCode <= 90) || 
-                    (e.keyCode >= 48 && e.keyCode <=57))
+                    (e.keyCode >= 48 && e.keyCode <=57)) {
                     add_to_page_stats(e.type);
-                else
+                } else {
                     add_to_page_stats(e.type, e.keyCode);
+                }
             });
 
             $(document).on("mousemove", function(e) {
@@ -531,9 +536,8 @@ $(document).ready(function () {
             });
 
             $(document).on("click", function(e) {
-                alert(get_path(e.target));
                 add_to_page_stats("click", {"x": e.clientX, "y": e.clientY,
-                    "button": e.which, "path": get_path(e.currentTarget)});
+                    "button": e.which, "path": get_path(e.target)});
             });
 
             $(window).on("focus blur", function(e) {
@@ -556,14 +560,14 @@ $(document).ready(function () {
 
         var stats_interval = $('body').data('stats-interval');
         var sendOnPagePing = function() {
-            if (stats_extended == undefined) { 
-                var append_string = "";
-            } else {
+            if (stats_extended) { 
                 add_to_page_stats("active_element",
                     get_path(document.activeElement));
                 var append_string = '&data=' + JSON.stringify(page_stats_data)
                     + '&res=' + window.innerHeight + '|' + window.innerWidth;
                 page_stats_data = new Array();
+            } else {
+                var append_string = "";
             }
             $.get(page_stats_baseurl + '?page=' + encodeURIComponent(location.href)
                     + append_string,
