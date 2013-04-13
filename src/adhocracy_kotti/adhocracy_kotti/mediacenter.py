d9067f1f1bc0d5e40d76c91151f6418bf3c74f94
Joscha Krutzki <joka@jokasis.de>
2013-04-13 18:17:22 +0200

    adhocracy_kotti: moved validators in extra module

--- src/adhocracy_kotti/adhocracy_kotti/mediacenter.py
+++ src/adhocracy_kotti/adhocracy_kotti/mediacenter.py
@@ -1,13 +1,12 @@
-import base64
-import binascii
+"""Medicenter rest web service"""
 from kotti.resources import Image
 from kotti.views.util import content_with_tags
 from kotti.views.image import ImageView
-from kotti.util import extract_from_settings
 from cornice import Service
 
 from adhocracy_kotti import schemata
 from adhocracy_kotti import utils
+from adhocracy_kotti import validate
 
 
 images = Service(
@@ -26,30 +25,9 @@ imagescale = Service(
     description="Service to get a scaled image binary data")
 
 
-def validate_image_data(request):
-    data_raw = request.validated.get("data", b"")
-    try:
-        data = base64.decodestring(data_raw)
-        request.validated["data"] = data
-    except (binascii.Error, UnicodeEncodeError) as e:
-        error = u"The image data is not valid base64 encoding: %s"
-        request.errors.add('body', 'data', error % e)
-
-
-def validate_api_token(request):
-    if not 'X-API-Token' in request.headers:
-        request.errors.add('header', 'X-API-Token',
-                           'You need to provied a valid token')
-    token = request.headers.get('X-API-Token', '')
-    valid_token = request.registry.settings['rest_api_token']
-    if token != valid_token:
-        request.errors.add('header', 'X-API-Token',
-                           'The token is invalid')
-
-
 #TODO use view classes instead of functions
 @images.post(schema=schemata.ImagePOST, accept="text/json",
-             validators=(validate_image_data,))
+             validators=(validate.validate_image_data,))
 def images_post(request):
     """Add new Image.
 
--- src/adhocracy_kotti/adhocracy_kotti/tests/test_mediacenter.py
+++ src/adhocracy_kotti/adhocracy_kotti/tests/test_mediacenter.py
@@ -19,37 +19,6 @@ IMAGEDATA_APPSTRUCT = {"filename": u"test_image",
                        }
 
 
-def test_validate_image_data_valid(dummy_request):
-    from adhocracy_kotti.mediacenter import validate_image_data
-    dummy_request.validated = {"data": b"binary_base64"}
-    validate_image_data(dummy_request)
-    assert(len(dummy_request.errors) == 0)
-
-
-def test_validate_image_data_invalid(dummy_request):
-    from adhocracy_kotti.mediacenter import validate_image_data
-    dummy_request.validated = {"data": u"wrong dötö"}
-    validate_image_data(dummy_request)
-    assert(len(dummy_request.errors) == 1)
-
-
-def test_validate_api_token_valid(dummy_request):
-    from adhocracy_kotti.mediacenter import validate_api_token
-    from adhocracy_kotti.testing import API_TOKEN
-    dummy_request.headers["X-API-Token"] = API_TOKEN
-    dummy_request.validated = {}
-    validate_api_token(dummy_request)
-    assert(len(dummy_request.errors) == 0)
-
-
-def test_validate_api_token_invalid(dummy_request):
-    from adhocracy_kotti.mediacenter import validate_api_token
-    dummy_request.headers["X-API-Token"] = "wrong token"
-    dummy_request.validated = {}
-    validate_api_token(dummy_request)
-    assert(len(dummy_request.errors) == 1)
-
-
 def test_images_post_one(root, request):  # pytest public fixtures: conftest.py
     from adhocracy_kotti.mediacenter import images_post
     data = copy.deepcopy(IMAGEDATA_APPSTRUCT)
