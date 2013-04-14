ae93c58445ee3bdc400f2f48c1b75f46e702c187
Joscha Krutzki <joka@jokasis.de>
2013-04-14 20:21:10 +0200

    added options to disable assigning mediafiles to proposals
    Conflicts:
    	etc/adhocracy.ini.in

--- src/adhocracy/controllers/proposal.py
+++ src/adhocracy/controllers/proposal.py
@@ -57,8 +57,8 @@ class ProposalCreateForm(ProposalNewForm):
                                   if_missing=False)
     wiki = validators.StringBool(not_empty=False, if_empty=False,
                                  if_missing=False)
-    image = All(ValidImageFileUpload(not_empty=False),
-                                     ValidFileUpload(not_empty=False),)
+    image = All(ValidImageFileUpload(not_empty=False, if_missing=None),
+                ValidFileUpload(not_empty=False),)
     chained_validators = [
         forms.UnusedProposalTitle(),
     ]
@@ -96,6 +96,7 @@ class ProposalFilterForm(formencode.Schema):
     proposals_state = validators.String(max=255, not_empty=False,
                                         if_empty=None, if_missing=None)
 
+
 class DelegateableBadgesForm(formencode.Schema):
     allow_extra_fields = True
     badge = formencode.foreach.ForEach(forms.ValidDelegateableBadge())
@@ -109,6 +110,8 @@ class ProposalController(BaseController):
     def __init__(self):
         super(ProposalController, self).__init__()
         c.api = h.adhocracy_service.RESTAPI()
+        c.allow_mediafiles = config.get_bool(
+            'adhocracy.delegateable_mediafiles', False)
 
     @RequireInstance
     @validate(schema=ProposalFilterForm(), post_only=False, on_get=True)
