f39c4370c466ab55924fe2da471732aa0ae40312
Nicolas Dietrich <nicodietrich@gmail.com>
2013-04-15 22:33:08 +0200

    pep8

--- src/adhocracy/lib/event/rss.py
+++ src/adhocracy/lib/event/rss.py
@@ -7,23 +7,22 @@ from adhocracy.lib.event import formatting
 
 
 def rss_feed(events, name, link, description):
-        rss = Feed(name, link.encode('utf-8'),
-                                 description)
+    rss = Feed(name, link.encode('utf-8'), description)
 
-        def event_item(event):
-            try:
-                item_link = event.event.link_path(event)
-            except:
-                item_link = link
+    def event_item(event):
+        try:
+            item_link = event.event.link_path(event)
+        except:
+            item_link = link
 
-            rss.add_item(title=u"%s %s" % (event.user.name,
-                         formatting.as_unicode(event)),
-                         link=item_link.encode('utf-8'),
-                         pubdate=event.time,
-                         description=event.text(),
-                         author_name=event.user.name,
-                         unique_id=item_link.encode('utf-8'))
+        rss.add_item(title=u"%s %s" % (event.user.name,
+                                       formatting.as_unicode(event)),
+                     link=item_link.encode('utf-8'),
+                     pubdate=event.time,
+                     description=event.text(),
+                     author_name=event.user.name,
+                     unique_id=item_link.encode('utf-8'))
 
-        response.content_type = 'application/rss+xml'
-        pager.NamedPager('rss', events, event_item, size=50).here()
-        return rss.writeString('utf-8')
+    response.content_type = 'application/rss+xml'
+    pager.NamedPager('rss', events, event_item, size=50).here()
+    return rss.writeString('utf-8')
