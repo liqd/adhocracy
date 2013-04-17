bc9b7f5c6acbe429265cc0835177578d57dc5799
Joscha Krutzki <joka@jokasis.de>
2013-04-17 20:38:15 +0200

    model: mediafiles to comments assignment.

--- src/adhocracy/migration/versions/085_add_mediafiles.py
+++ src/adhocracy/migration/versions/085_add_mediafiles.py
@@ -37,16 +37,30 @@ delegateable_mediafiles_table = Table(
 )
 
 
+comment_mediafiles_table = Table(
+    'comment_mediafiles', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('mediafile_id', Integer, ForeignKey('mediafile.id'),
+           nullable=False),
+    Column('comment_id', Integer, ForeignKey('comment.id'),
+           nullable=False),
+    Column('create_time', DateTime, default=datetime.utcnow),
+    Column('creator_id', Integer, ForeignKey('user.id'), nullable=False)
+)
+
+
 def upgrade(migrate_engine):
     #use sqlalchemy-migrate database connection
     metadata.bind = migrate_engine
     #autoload needed tables
     instance_table = Table('instance', metadata, autoload=True)
     delegateable_table = Table('delegateable', metadata, autoload=True)
+    comment_table = Table('comment', metadata, autoload=True)
 
     #create/recreate the table
     mediafile_table.create()
     delegateable_mediafiles_table.create()
+    comment_mediafiles_table.create()
 
 
 def downgrade(migrate_engine):
--- src/adhocracy/tests/model/test_mediafiles.py
+++ src/adhocracy/tests/model/test_mediafiles.py
@@ -45,7 +45,7 @@ class TestDelegateableController(TestController):
         from adhocracy.model import meta
         creator, delegateable, mediafile = self._make_content()
         # create the delegateable mediafile
-        mediafile.assign(delegateable, creator)
+        mediafile.assignDelegateable(delegateable, creator)
         delegateablemediafile = \
             meta.Session.query(DelegateableMediaFiles).first()
         self.assertTrue(delegateablemediafile.creator is creator)
@@ -66,7 +66,7 @@ class TestDelegateableController(TestController):
         from adhocracy.model.mediafile import DelegateableMediaFiles
         from adhocracy.model import meta
         creator, delegateable, mediafile = self._make_content()
-        mediafile.assign(delegateable, creator)
+        mediafile.assignDelegateable(delegateable, creator)
         #remove mediafile from delegateable
         delegateable.mediafiles.remove(mediafile)
         self.assertEqual(delegateable.mediafiles, [])
@@ -78,9 +78,69 @@ class TestDelegateableController(TestController):
         from adhocracy.model.mediafile import DelegateableMediaFiles
         from adhocracy.model import meta
         creator, delegateable, mediafile = self._make_content()
-        mediafile.assign(delegateable, creator)
+        mediafile.assignDelegateable(delegateable, creator)
         #remove delegateable from mediafile
         mediafile.delegateables.remove(delegateable)
         self.assertEqual(mediafile.delegateables, [])
         self.assertEqual(delegateable.mediafiles, [])
         self.assertEqual(meta.Session.query(DelegateableMediaFiles).count(), 0)
+
+
+class TestCommentController(TestController):
+
+    def _make_content(self):
+        """Returns creator, comment and mediafile"""
+        from adhocracy.model.comment import Comment
+        from adhocracy.model.proposal import Proposal
+        from adhocracy.model.instance import Instance
+        from adhocracy.model.mediafile import MediaFile
+        instance = Instance.find('test')
+        creator = tt_make_user('creator')
+        topic = Proposal.create(instance, u"labeld", creator)
+        comment = Comment.create(u"text", creator, topic)
+        mediafile = MediaFile.create(u'name')
+
+        return creator, comment, mediafile
+
+    def test_commentmediafiles_created(self):
+        #setup
+        from adhocracy.model.mediafile import CommentMediaFiles
+        from adhocracy.model import meta
+        creator, comment, mediafile = self._make_content()
+        # create the comment mediafile
+        mediafile.assignComment(comment, creator)
+        commentmediafile = \
+            meta.Session.query(CommentMediaFiles).first()
+        self.assertTrue(commentmediafile.creator is creator)
+        self.assertTrue(commentmediafile.comment is comment)
+        self.assertTrue(commentmediafile.mediafile is mediafile)
+        # test the references on the mediafiled comment
+        self.assertEqual(comment.mediafiles, [mediafile])
+        # test the references on the mediafile
+        self.assertTrue(comment.mediafiles[0].comments
+                        == mediafile.comments
+                        == [comment])
+
+    def test_remove_mediafile_from_comment(self):
+        #setup
+        from adhocracy.model.mediafile import CommentMediaFiles
+        from adhocracy.model import meta
+        creator, comment, mediafile = self._make_content()
+        mediafile.assignComment(comment, creator)
+        #remove mediafile from comment
+        comment.mediafiles.remove(mediafile)
+        self.assertEqual(comment.mediafiles, [])
+        self.assertEqual(mediafile.comments, [])
+        self.assertEqual(meta.Session.query(CommentMediaFiles).count(), 0)
+
+    def test_remove_comment_from_mediafile(self):
+        #setup
+        from adhocracy.model.mediafile import CommentMediaFiles
+        from adhocracy.model import meta
+        creator, comment, mediafile = self._make_content()
+        mediafile.assignComment(comment, creator)
+        #remove comment from mediafile
+        mediafile.comments.remove(comment)
+        self.assertEqual(mediafile.comments, [])
+        self.assertEqual(comment.mediafiles, [])
+        self.assertEqual(meta.Session.query(CommentMediaFiles).count(), 0)
