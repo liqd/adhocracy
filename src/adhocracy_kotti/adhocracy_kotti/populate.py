a1f011d0818f263fbc5177d571bca43c2f7ca270
Joscha Krutzki <joka@jokasis.de>
2013-04-17 16:35:26 +0200

    start adding tests for the mediacenter module

--- src/adhocracy_kotti/adhocracy_kotti/populate.py
+++ src/adhocracy_kotti/adhocracy_kotti/populate.py
@@ -21,29 +21,48 @@ def populate():
         root = Document(**_ROOT_ATTRS)
         root.__acl__ = SITE_ACL
         DBSession.add(root)
+        root['de'] = Document(**_DE_ATTRS)
+        root['en'] = Document(**_EN_ATTRS)
         root['mediacenter'] = Document(**_MEDIACENTER_ATTRS)
 
         wf = get_workflow(root)
         if wf is not None:
             DBSession.flush()  # Initializes workflow
             wf.transition_to_state(root, None, u'public')
-            wf.transition_to_state(root["mediacenter"], None, u'public')
+            wf.transition_to_state(root["de"], None, u'public')
+            wf.transition_to_state(root["en"], None, u'public')
 
     populate_users()
 
 
 _ROOT_ATTRS = dict(
     name=u'',  # (at the time of writing) root must have empty name!
-    title=u'Welcome to Kotti',
-    description=u'Congratulations! You have successfully installed Kotti.',
-    body=u""" """
+    title=u'Adhocracy/Kotti root node',
+    description=u'This is the root of all static content.',
+    body=u""
+)
+
+
+_DE_ATTRS = dict(
+    name=u'de',
+    title=u'DE translations',
+    description=u'Folder for German translations.',
+    body=u""
+)
+
+
+_EN_ATTRS = dict(
+    name=u'en',
+    title=u'EN translations',
+    description=u'Folder for English translations.',
+    body=u""
 )
 
 
 _MEDIACENTER_ATTRS = dict(
     name=u'mediacenter',
     title=u'Mediacenter',
-    description=u'Folder to stores media center files (webservice for'
-                ' adhocracy). DONT CHANGE files here manually.',
-    body=u""" """
+    description=u'Folder to store media center files (webservice for'
+                u' Adhocracy). DON\'T CHANGE files here manually.',
+    body=u""
 )
