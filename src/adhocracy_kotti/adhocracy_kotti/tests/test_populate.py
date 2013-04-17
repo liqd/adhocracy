bffed24188a17e82771a41147b545ccb133c322f
Joscha Krutzki <joka@jokasis.de>
2013-04-17 16:32:49 +0200

    moved staticpages module to new branch

--- src/adhocracy_kotti/adhocracy_kotti/tests/test_populate.py
+++ src/adhocracy_kotti/adhocracy_kotti/tests/test_populate.py
@@ -4,4 +4,4 @@ from kotti.resources import get_root
 def test_populate(populate):
     root = get_root()
     # test keys
-    assert root.keys() == [u'de', u'en', u'mediacenter']
+    assert root.keys() == [u'mediacenter']
