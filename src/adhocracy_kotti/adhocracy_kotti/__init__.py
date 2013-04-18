82906e51d6ffc6bce25931a9ada032b442be0494
Joscha Krutzki <joka@jokasis.de>
2013-04-18 15:56:45 +0200

    adhocracy_kotti_medicenter: new image scales

--- src/adhocracy_kotti/adhocracy_kotti/__init__.py
+++ src/adhocracy_kotti/adhocracy_kotti/__init__.py
@@ -4,8 +4,9 @@ def kotti_configure(settings):
 
     settings['kotti.image_scales.icon'] = '16x16'  # <max_width>x<max_height>
     settings['kotti.image_scales.thumb'] = '48x48'
+    settings['kotti.image_scales.small'] = '128x128'
     settings['kotti.image_scales.logo'] = '300x90'
-    settings['kotti.image_scales.middle'] = '114x600'
+    settings['kotti.image_scales.middle'] = '632x632'
     settings['kotti.image_scales.large'] = '1004x1004'
 
 
