507432c81971be9e2a68b5a9aef547a7c0678071
Philipp Hagemeister <phihag@phihag.de>
2013-04-19 18:33:38 +0200

    Fix default boolean value in migration script for MySQL (Fixes #hhucn/adhocracy.hhu_theme#250)

--- src/adhocracy/migration/versions/061_optional_massmessage_footer.py
+++ src/adhocracy/migration/versions/061_optional_massmessage_footer.py
@@ -8,5 +8,5 @@ def upgrade(migrate_engine):
 
     message_table = Table('message', meta, autoload=True)
     col = Column('include_footer', Boolean, nullable=False, default=True,
-                 server_default="true")
+                 server_default="1")
     col.create(message_table)
