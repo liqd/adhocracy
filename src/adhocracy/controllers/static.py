38e9072ab85cc1232b9debfe98ab64b40fb818c2
Nicolas Dietrich <nicodietrich@gmail.com>
2013-04-28 23:21:02 +0200

    Modify staticpage backend API to consume multiple languages
    Implement the fallback mechanism on backend side

--- src/adhocracy/controllers/static.py
+++ src/adhocracy/controllers/static.py
@@ -80,7 +80,7 @@ class StaticController(BaseController):
     @guard_perms
     def edit(self, key, lang, errors=None, format=u'html'):
         backend = get_backend()
-        sp = backend.get(key, lang)
+        sp = backend.get(key, [lang])
         if not sp:
             return ret_abort(_('Cannot find static page to edit'), code=404)
         data = {'staticpage': sp}
@@ -98,7 +98,7 @@ class StaticController(BaseController):
     @csrf.RequireInternalRequest(methods=['POST'])
     def update(self, key, lang):
         backend = get_backend()
-        sp = backend.get(key, lang)
+        sp = backend.get(key, [lang])
         if not sp:
             return ret_abort(_('Cannot find static page to edit'), code=404)
 
