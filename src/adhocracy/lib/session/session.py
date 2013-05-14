78f3658d3910e390d2d493d4390ad5fdec778cd8
Philipp Hagemeister <phihag@phihag.de>
2013-05-14 19:59:26 +0200

    Only use a single cookie for session data

--- src/adhocracy/lib/session/session.py
+++ src/adhocracy/lib/session/session.py
@@ -4,11 +4,11 @@ except ImportError:  # Python < 3
     from Cookie import SimpleCookie
 import logging
 
-from adhocracy.lib.cookie import get_cookies
+from adhocracy.lib.cookie import get_cookie
 from adhocracy.lib.session.converter import SignedValueConverter
 
 log = logging.getLogger(name=__name__)
-_PREFIX = u'adhocracy_session_'
+COOKIE_KEY = u'adhocracy_session'
 
 
 def get_secret(config):
@@ -31,47 +31,47 @@ class Session(dict):
             converter_class = SignedValueConverter
 
         if 'adhocracy.session.lifetime' in config:
-            self._max_age = int(config.get('adhocracy.session.lifetime'))
+            self._max_age = int(config['adhocracy.session.lifetime'])
         else:
             self._max_age = None
 
-        self._changed = set()
-        self._deleted = set()
+        self._changed = False
 
         secret = get_secret(config)
         self._converter = converter_class(secret)
         self._load_from_cookie(environ)
 
     def __setitem__(self, key, value):
-        self._changed.add(key)
-        self._deleted.discard(key)
+        self._changed = True
         return super(Session, self).__setitem__(key, value)
 
     def __delitem__(self, key):
-        self._changed.discard(key)
-        self._deleted.add(key)
+        self._changed = True
         super(Session, self).__delitem__(key)
 
     def _load_from_cookie(self, environ):
         cookie = SimpleCookie(environ.get("HTTP_COOKIE"))
-        if cookie:
-            for key, c in cookie.items():
-                if not key.startswith(_PREFIX):
-                    continue
-                key = key[len(_PREFIX):]
+        if not cookie:
+            return
+        cookieval = cookie.get(COOKIE_KEY)
+        if not cookieval:
+            return
 
-                val = self._converter.decode(c.value)
-                if val is not None:
-                    super(Session, self).__setitem__(key, val)
+        val = self._converter.decode(cookieval)
+        if not val:
+            return
+        assert isinstance(val, dict)
+
+        self.clear()
+        self.update(val)
+        self._changed = False
 
     def set_cookies_in(self, headers):
-        changed = dict((_PREFIX + k, self._converter.encode(self[k]))
-                       for k in self._changed)
-        headers.extend(get_cookies(changed, max_age=self._max_age))
-
-        deleted = dict((_PREFIX + k, 'invalid')
-                       for k in self._deleted)
-        headers.extend(get_cookies(deleted, max_age='delete'))
+        if not self._changed:
+            return
+        val = self._converter.encode(self)
+        c = get_cookie(COOKIE_KEY, val, max_age=self._max_age)
+        headers.append(c)
 
     def delete(self):
         self._deleted.update(self.keys())
