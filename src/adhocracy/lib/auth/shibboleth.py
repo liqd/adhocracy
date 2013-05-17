1d9e9f2b7a2259f19a48ab0e0f41439ba5224648
Nicolas Dietrich <nicodietrich@gmail.com>
2013-05-18 00:14:39 +0200

    Add userbade mappings contains and contains_substring

--- src/adhocracy/lib/auth/shibboleth.py
+++ src/adhocracy/lib/auth/shibboleth.py
@@ -9,9 +9,29 @@ def get_userbadge_mapping(config=config):
 
 
 def _attribute_equals(request, key, value):
+    """
+    exact match
+    """
     return request.headers.get(key) == value
+
+
+def _attribute_contains(request, key, value):
+    """
+    contains element
+    """
+    elements = (e.strip() for e in request.headers.get(key).split(','))
+    return value in elements
+
+
+def _attribute_contains_substring(request, key, value):
+    """
+    contains substring
+    """
+    return value in request.headers.get(key)
 
 
 USERBADGE_MAPPERS = {
     'attribute_equals': _attribute_equals,
+    'attribute_contains': _attribute_contains,
+    'attribute_contains_substring': _attribute_contains_substring,
 }
