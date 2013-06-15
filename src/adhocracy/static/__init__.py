8ec7cf8bc42d147960cece26e889e8bb4d9c44af
Philipp Hagemeister <phihag@phihag.de>
2013-06-16 00:32:53 +0200

    [openid selector] move JS into main pipeline as well

--- src/adhocracy/static/__init__.py
+++ src/adhocracy/static/__init__.py
@@ -57,6 +57,8 @@ spectrum = Resource(misc_library, 'spectrum/spectrum.js',
 select_hierarchy = Resource(misc_library, 'jquery.select-hierarchy.js',
                             minified='jquery.select-hierarchy.min.js',
                             depends=[jquery])
+openid_selector = Resource(misc_library, 'openid.js',
+                            depends=[jquery])
 
 
 # --[ adhocracy ]-----------------------------------------------------------
@@ -64,7 +66,8 @@ select_hierarchy = Resource(misc_library, 'jquery.select-hierarchy.js',
 adhocracy_library = Library('adhocracy', 'javascripts')
 adhocracy = Resource(adhocracy_library, 'adhocracy.js',
                      depends=[jquery, bootstrap_js, elastic,
-                              placeholder, modernizr, jquerytools])
+                              placeholder, modernizr, jquerytools,
+                              openid_selector])
 
 
 # --[ knockout ]------------------------------------------------------------
