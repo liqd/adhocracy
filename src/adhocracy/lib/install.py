4776dc6c9b31122d2adb43a88ff4f08bd36883df
Nicolas Dietrich <nicodietrich@gmail.com>
2013-06-24 01:25:15 +0200

    Add second permission level for static page views
    This adds a permission `static.show_private` which can be requested by
    static page backends by setting the static page `private` attribute
    `True`.

--- src/adhocracy/lib/install.py
+++ src/adhocracy/lib/install.py
@@ -126,6 +126,7 @@ def setup_entities(config, initial_setup):
         u'proposal.view': [anonymous],
         u'proposal.message': [moderator],
         u'static.show': [anonymous],
+        u'static.show_private': [admins],
         u'tag.create': [advisor],
         u'tag.delete': [advisor],
         u'tag.show': [anonymous],
