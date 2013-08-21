9c9691f2cdaa686f05ae2ffb2509fb906b4e80c2
Nicolas Dietrich <nicodietrich@gmail.com>
2013-08-21 17:48:49 +0200

    Allow to send user messages from global user profile
    This has previously only been allowed from within instance profiles for
    no good reason.

--- src/adhocracy/controllers/message.py
+++ src/adhocracy/controllers/message.py
@@ -10,7 +10,6 @@ from adhocracy import model
 from adhocracy.lib import helpers as h
 from adhocracy.lib.auth import require
 from adhocracy.lib.base import BaseController
-from adhocracy.lib.instance import RequireInstance
 from adhocracy.lib.templating import render
 from adhocracy.lib.util import get_entity_or_abort
 
@@ -25,7 +24,6 @@ class MessageCreateForm(Schema):
 
 class MessageController(BaseController):
 
-    @RequireInstance
     def new(self, id, format='html', errors={}):
         c.page_user = get_entity_or_abort(model.User, id)
         require.user.message(c.page_user)
@@ -33,7 +31,6 @@ class MessageController(BaseController):
         return htmlfill.render(html, defaults=request.params,
                                errors=errors, force_defaults=False)
 
-    @RequireInstance
     def create(self, id, format='html'):
         c.page_user = get_entity_or_abort(model.User, id)
         require.user.message(c.page_user)
@@ -50,7 +47,8 @@ class MessageController(BaseController):
             headers['Reply-To'] = c.user.email
 
         from adhocracy.lib.mail import to_user
-        subject = _("[%s] Message from %s: %s") % (c.instance.label,
+        label = h.site.name() if c.instance is None else c.instance.label
+        subject = _("[%s] Message from %s: %s") % (label,
                                                    c.user.name, c.subject)
         to_user(c.page_user, subject, message, headers=headers)
 
