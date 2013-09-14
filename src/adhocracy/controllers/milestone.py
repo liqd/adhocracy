liqd/adhocracy

commit 5c02f22acc474ae8e9d2261d3ae6b9f881f7b003

use format=overlay for overlays with a stripped down root template

src/adhocracy/controllers/milestone.py
@@ -78,7 +78,10 @@ class MilestoneController(BaseController):
         c.tile = tiles.instance.InstanceTile(c.instance)
         c.tutorial = 'milestone_index'
         c.tutorial_intro = _('tutorial_milestones_tab')
-        return render("/milestone/index.html")
+        if format == 'overlay':
+            return render("/milestone/index.html", overlay=True)
+        else:
+            return render("/milestone/index.html")
 
     @RequireInstance
     @validate(schema=MilestoneNewForm(), form='bad_request',
@@ -185,7 +188,11 @@ class MilestoneController(BaseController):
         self._common_metadata(c.milestone)
         c.tutorial_intro = _('tutorial_milestone_details_tab')
         c.tutorial = 'milestone_show'
-        return render("/milestone/show.html")
+
+        if format == overlay:
+            return render("/milestone/show.html", overlay=True)
+        else:
+            return render("/milestone/show.html")
 
     @RequireInstance
     def ask_delete(self, id):

src/adhocracy/controllers/comment.py
@@ -68,10 +68,11 @@ class CommentController(BaseController):
             sorts={_("oldest"): sorting.entity_oldest,
                    _("newest"): sorting.entity_newest},
             default_sort=sorting.entity_newest)
+
         if format == 'json':
             return render_json(c.comments_pager)
-
-        return self.not_implemented(format=format)
+        else:
+            return self.not_implemented(format=format)
 
     @RequireInstance
     @validate(schema=CommentNewForm(), form="bad_request",
@@ -86,6 +87,7 @@ class CommentController(BaseController):
             require.comment.reply(c.reply)
         else:
             require.comment.create_on(c.topic)
+
         if format == 'ajax':
             html = self._render_ajax_create_form(c.reply, c.topic, c.variant)
         else:
@@ -135,7 +137,8 @@ class CommentController(BaseController):
             redirect(request.params.get('ret_url') + "#c" + str(comment.id))
         if format != 'html':
             return ret_success(entity=comment, format=format)
-        return ret_success(entity=comment, format='fwd')
+        else:
+            return ret_success(entity=comment, format='fwd')
 
     @RequireInstance
     def edit(self, id, format='html'):
@@ -144,7 +147,10 @@ class CommentController(BaseController):
         if format == 'ajax':
             return render_def('/comment/tiles.html', 'edit_form',
                               {'comment': c.comment})
-        return render('/comment/edit.html')
+        elif format == 'overlay':
+            return render('/comment/edit.html', overlay=True)
+        else:
+            return render('/comment/edit.html')
 
     @RequireInstance
     @csrf.RequireInternalRequest(methods=['POST'])
@@ -171,7 +177,8 @@ class CommentController(BaseController):
             redirect(request.params.get('ret_url') + "#c" + str(c.comment.id))
         if format != 'html':
             return ret_success(entity=c.comment, format=format)
-        return ret_success(entity=c.comment, format='fwd')
+        else:
+            return ret_success(entity=c.comment, format='fwd')
 
     @RequireInstance
     def show(self, id, format='html'):
@@ -213,11 +220,15 @@ class CommentController(BaseController):
             sorts={_("oldest"): sorting.entity_oldest,
                    _("newest"): sorting.entity_newest},
             default_sort=sorting.entity_newest)
-        if format == 'overlay':
-            return c.revisions_pager.render_pager()
+
         if format == 'json':
             return render_json(c.revisions_pager)
-        return render('/comment/history.html')
+        elif format == 'ajax':
+            return c.revisions_pager.render_pager()
+        elif format == 'overlay':
+            return render('/comment/history.html', overlay=True)
+        else:
+            return render('/comment/history.html')
 
     @RequireInstance
     @csrf.RequireInternalRequest()

src/adhocracy/controllers/delegation.py
@@ -41,10 +41,11 @@ class DelegationController(BaseController):
             c.users = model.User.all(instance=c.instance)
             response.content_type = "text/plain"
             return render("/delegation/graph.dot")
-        if format == 'json':
+        elif format == 'json':
             c.delegations_pager = pager.delegations(c.delegations)
             return render_json(c.delegations_pager)
-        return self.not_implemented(format=format)
+        else:
+            return self.not_implemented(format=format)
 
     @RequireInstance
     @guard.delegation.create()
@@ -122,8 +123,10 @@ class DelegationController(BaseController):
 
         if format == 'json':
             return render_json((c.delegation, c.decisions_pager))
-
-        return render("delegation/show.html")
+        elif format == 'overlay':
+            return render("delegation/show.html", overlay=True)
+        else:
+            return render("delegation/show.html")
 
     @RequireInstance
     def edit(self, format='html'):

src/adhocracy/controllers/event.py
@@ -32,4 +32,7 @@ class EventController(BaseController):
 
         c.event_pager = NamedPager('events', query.all(),
                                    tiles.event.row, count=50)
-        return render('/event/all.html')
+        if format == 'overlay':
+            return render('/event/all.html', overlay=True)
+        else:
+            return render('/event/all.html')

src/adhocracy/controllers/instance.py
@@ -158,7 +158,10 @@ class InstanceController(BaseController):
             return render_json(c.instance_pager)
 
         c.tile = tiles.instance.InstanceTile(c.instance)
-        return render("/instance/index.html")
+        if format == 'overlay':
+            return render("/instance/index.html", overlay=True)
+        else:
+            return render("/instance/index.html")
 
     @guard.instance.create()
     def new(self):
@@ -200,8 +203,7 @@ class InstanceController(BaseController):
 
         if format == 'json':
             return render_json(c.page_instance)
-
-        if format == 'rss':
+        elif format == 'rss':
             return self.activity(id, format)
 
         if c.page_instance != c.instance:
@@ -258,7 +260,11 @@ class InstanceController(BaseController):
 
         c.tutorial_intro = _('tutorial_instance_show_intro')
         c.tutorial = 'instance_show'
-        return render("/instance/show.html")
+
+        if format == 'overlay':
+            return render("/instance/show.html", overlay=True)
+        else:
+            return render("/instance/show.html")
 
     @RequireInstance
     def activity(self, id, format='html'):
@@ -278,7 +284,11 @@ class InstanceController(BaseController):
 
         c.tile = tiles.instance.InstanceTile(c.page_instance)
         c.events_pager = pager.events(events)
-        return render("/instance/activity.html")
+
+        if format == 'overlay':
+            return render("/instance/activity.html", overlay=True)
+        else:
+            return render("/instance/activity.html")
 
     @RequireInstance
     def edit(self, id):
@@ -321,10 +331,10 @@ class InstanceController(BaseController):
                         'title': badge.title,
                         'checked': badge.id in checked} for badge in c.badges]}
             return render_json(json)
-
-        return formencode.htmlfill.render(
-            render("/instance/badges.html"),
-            defaults=defaults)
+        else:
+            return formencode.htmlfill.render(
+                render("/instance/badges.html"),
+                defaults=defaults)
 
     @validate(schema=InstanceBadgesForm(), form='badges')
     @guard.perm("global.admin")
@@ -429,8 +439,7 @@ class InstanceController(BaseController):
                  'label': h.literal(_(group.group_name)),
                  'selected': group.code == c.default_group})
 
-        rendered = render("/instance/settings_general.html")
-        return rendered
+        return render("/instance/settings_general.html")
 
     @RequireInstance
     def settings_general(self, id):

src/adhocracy/controllers/page.py
@@ -114,7 +114,11 @@ class PageController(BaseController):
                               key=lambda (k, c, v): k.name)
         c.tutorial_intro = _('tutorial_norms_overview_tab')
         c.tutorial = 'page_index'
-        return render("/page/index.html")
+
+        if format == 'overlay':
+            return render("/page/index.html", overlay=True)
+        else:
+            return render("/page/index.html")
 
     @RequireInstance
     @guard.page.create()
@@ -520,7 +524,7 @@ class PageController(BaseController):
         return render("/page/show.html")
 
     @RequireInstance
-    def history(self, id, variant=model.Text.HEAD, text=None, format=None):
+    def history(self, id, variant=model.Text.HEAD, text=None, format='html'):
         c.page, c.text, c.variant = self._get_page_and_text(id, variant, text)
         require.page.show(c.page)
         if c.text is None:
@@ -536,8 +540,10 @@ class PageController(BaseController):
         c.tile = tiles.page.PageTile(c.page)
         self._common_metadata(c.page, c.text)
 
-        if format == 'overlay':
+        if format == 'ajax':
             return c.texts_pager.here()
+        elif format == 'overlay':
+            return render('/page/history.html', overlay=True)
         else:
             return render('/page/history.html')
 

src/adhocracy/controllers/poll.py
@@ -62,8 +62,8 @@ class PollController(BaseController):
 
         if format == 'json':
             return render_json(poll)
-
-        return self.not_implemented(format=format)
+        else:
+            return self.not_implemented(format=format)
 
     @RequireInstance
     @RequireInternalRequest()
@@ -89,8 +89,8 @@ class PollController(BaseController):
             return render_json(dict(decision=decision,
                                     score=c.poll.tally.score,
                                     votedetail=vdetail))
-
-        redirect(h.entity_url(c.poll.subject))
+        else:
+            redirect(h.entity_url(c.poll.subject))
 
     @RequireInstance
     @RequireInternalRequest()
@@ -133,14 +133,12 @@ class PollController(BaseController):
             return render_json(dict(decision=decision,
                                     tally=tally.to_dict(),
                                     votedetail=vdetail))
-
-        if format == 'overlay':
+        elif format == 'ajax':
             return self.widget(id, format=self.form_result.get('cls'))
-
-        if c.poll.action == model.Poll.SELECT:
+        elif c.poll.action == model.Poll.SELECT:
             redirect(h.entity_url(c.poll.selection))
-
-        redirect(h.entity_url(c.poll.subject))
+        else:
+            redirect(h.entity_url(c.poll.subject))
 
     @RequireInstance
     @validate(schema=PollVotesFilterForm(), post_only=False, on_get=True)
@@ -166,15 +164,16 @@ class PollController(BaseController):
             decisions = filter(lambda d: d.result == result_form, decisions)
         c.decisions_pager = pager.scope_decisions(decisions)
 
-        if format == 'overlay':
+        if format == 'json':
+            return render_json(c.decisions_pager)
+        elif format == 'ajax':
             return render_def('/pager.html', 'overlay_pager',
                               pager=c.decisions_pager,
                               render_facets=False)
-
-        if format == 'json':
-            return render_json(c.decisions_pager)
-
-        return render("/poll/votes.html")
+        elif format == 'overlay':
+            return render("/poll/votes.html", overlay=True)
+        else:
+            return render("/poll/votes.html")
 
     def ask_delete(self, id):
         c.poll = self._get_open_poll(id)

src/adhocracy/controllers/proposal.py
@@ -103,7 +103,11 @@ class ProposalController(BaseController):
         c.tile = tiles.instance.InstanceTile(c.instance)
         c.tutorial_intro = _('tutorial_proposal_overview_tab')
         c.tutorial = 'proposal_index'
-        return render("/proposal/index.html")
+
+        if format == 'overlay':
+            return render("/proposal/index.html", overlay=True)
+        else:
+            return render("/proposal/index.html")
 
     def _set_categories(self):
         categories = model.CategoryBadge.all(
@@ -312,8 +316,7 @@ class ProposalController(BaseController):
 
         if format == 'rss':
             return self.activity(id, format)
-
-        if format == 'json':
+        elif format == 'json':
             return render_json(c.proposal)
 
         c.tile = tiles.proposal.ProposalTile(c.proposal)
@@ -336,7 +339,11 @@ class ProposalController(BaseController):
                 h.base_url('/stats/read_comments'),
                 urllib.urlencode({'path':
                                   h.entity_url(c.proposal).encode('utf-8')}))
-        return render("/proposal/show.html")
+
+        if format == 'overlay':
+            return render("/proposal/show.html", overlay=True)
+        else:
+            return render("/proposal/show.html")
 
     @RequireInstance
     def history(self, id, format="html"):
@@ -351,8 +358,10 @@ class ProposalController(BaseController):
             default_sort=sorting.entity_newest)
 
         self._common_metadata(c.proposal)
-        if format == 'overlay':
-            return render_def('/proposal/history.html', 'overlay_content')
+        if format == 'ajax':
+            return render_def('/proposal/history.html', 'content')
+        elif format == 'overlay':
+            return render('/proposal/history.html', overlay=True)
         else:
             return render('/proposal/history.html')
 
@@ -368,7 +377,10 @@ class ProposalController(BaseController):
 
         c.tile = tiles.proposal.ProposalTile(c.proposal)
         self._common_metadata(c.proposal)
-        return render("/proposal/delegations.html")
+        if format == 'overlay':
+            return render("/proposal/delegations.html", overlay=True)
+        else:
+            return render("/proposal/delegations.html")
 
     @RequireInstance
     def activity(self, id, format='html'):
@@ -387,14 +399,21 @@ class ProposalController(BaseController):
         c.tile = tiles.proposal.ProposalTile(c.proposal)
         c.events_pager = pager.events(events)
         self._common_metadata(c.proposal)
-        return render("/proposal/activity.html")
+        if format == 'overlay':
+            return render("/proposal/activity.html", overlay=True)
+        else:
+            return render("/proposal/activity.html")
 
     @RequireInstance
     def ask_delete(self, id):
         c.proposal = get_entity_or_abort(model.Proposal, id)
         require.proposal.delete(c.proposal)
         c.tile = tiles.proposal.ProposalTile(c.proposal)
-        return render('/proposal/ask_delete.html')
+
+        if format == 'overlay':
+            return render('/proposal/ask_delete.html', overlay=True)
+        else:
+            return render('/proposal/ask_delete.html')
 
     @RequireInstance
     @csrf.RequireInternalRequest()
@@ -502,7 +521,7 @@ class ProposalController(BaseController):
                     '_tok': csrf.token_id(),
                     'thumbnailbadge': default_thumbnail,
                     }
-        if format == 'ajax':
+        if format == 'ajax': # REFACT shouldn't this be 'json'?
             checked = [badge.id for badge in c.proposal.badges]
             checked_thumbnail = default_thumbnail
             json = {'title': c.proposal.title,
@@ -521,10 +540,10 @@ class ProposalController(BaseController):
                         c.thumbnailbadges]
                     }
             return render_json(json)
-
-        return formencode.htmlfill.render(
-            render("/proposal/badges.html"),
-            defaults=defaults)
+        else:
+            return formencode.htmlfill.render(
+                render("/proposal/badges.html"),
+                defaults=defaults)
 
     @RequireInternalRequest()
     @validate(schema=DelegateableBadgesForm(), form='badges')
@@ -572,6 +591,7 @@ class ProposalController(BaseController):
                                                       proposal.thumbnails),
                    }
             return render_json(obj)
-        if redirect_to_proposals:
+        elif redirect_to_proposals:
             redirect("/proposal")
-        redirect(h.entity_url(proposal))
+        else:
+            redirect(h.entity_url(proposal))

commit 914b7416efb2a0973af9d084a0a3e730b5f04d21

Fix milestone detail view
This got broken in 5c02f22acc474ae8e9d2261d3ae6b9f881f7b003.

src/adhocracy/controllers/milestone.py
@@ -189,10 +189,7 @@ class MilestoneController(BaseController):
         c.tutorial_intro = _('tutorial_milestone_details_tab')
         c.tutorial = 'milestone_show'
 
-        if format == overlay:
-            return render("/milestone/show.html", overlay=True)
-        else:
-            return render("/milestone/show.html")
+        return render("/milestone/show.html", overlay=(format == 'overlay'))
 
     @RequireInstance
     def ask_delete(self, id):