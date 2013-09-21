10356dee8af77ef073e205f3edc51b0bc69094e1
Nicolas Dietrich <nicodietrich@gmail.com>
2013-09-21 17:04:54 +0200

    Fixup instance.frozen migration
    Fixup of 0713525067e2fc9121a7e56eba289fe767fd3c55

--- src/adhocracy/migration/versions/072_add_delegateable_frozen.py
+++ src/adhocracy/migration/versions/072_add_delegateable_frozen.py
@@ -6,5 +6,5 @@ def upgrade(migrate_engine):
     meta = MetaData(bind=migrate_engine)
 
     table = Table('delegateable', meta, autoload=True)
-    col = Column('frozen', Boolean, default=0)
+    col = Column('frozen', Boolean, default=False)
     col.create(table)
