d6728676e39a82f84bfd4fa6cf91586d16386e4f
Nicolas Dietrich <nicodietrich@gmail.com>
2013-11-16 23:44:50 +0100

    use Uri.js in adhocracy.overlay.rebindCameFrom
    This
    * simplifies code and
    * allows to replace the came_from parameter if it already exists instead
    of stupidly appending.

--- src/adhocracy/static/javascripts/adhocracy.js
+++ src/adhocracy/static/javascripts/adhocracy.js
@@ -266,13 +266,7 @@ var adhocracy = adhocracy || {};
             if (val === undefined) {
                 return undefined;
             }
-            var separator;
-            if (val.indexOf('?') !== -1) {
-                separator = '&';
-            } else {
-                separator = '?';
-            }
-            return val + separator + 'came_from=' + encodeURIComponent(came_from);
+            return new Uri(val).replaceQueryParam('came_from', came_from);
         };
         this.getOverlay().find('.patch_camefrom').attr({
             'action': patch_camefrom,
