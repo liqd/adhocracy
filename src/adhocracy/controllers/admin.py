76c7eae9ba606c999e78649bf04825aea7090cd2
Tobias Bengfort <tobias.bengfort@liqd.de>
2013-11-26 14:03:10 +0100

    fixup a5e938f3

--- src/adhocracy/controllers/admin.py
+++ src/adhocracy/controllers/admin.py
@@ -150,7 +150,7 @@ class AdminController(BaseController):
         if request.method == "POST":
             try:
                 self.form_result = UserImportForm().to_python(request.params,
-                    state={'global_admin': has('global.admin'))
+                    state={'global_admin': has('global.admin')})
                 # a proposal that this norm should be integrated with
                 data = user_import(self.form_result['users_csv'],
                                    self.form_result['email_subject'],
