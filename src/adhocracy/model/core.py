51514cc8facf0e423b6fcd8f6803339b9f5835b5
Nicolas Dietrich <nicodietrich@gmail.com>
2014-01-10 17:13:00 +0100

    Add MutableList (required for SQLAlchemy >= 0.8)

--- src/adhocracy/model/core.py
+++ src/adhocracy/model/core.py
@@ -45,3 +45,60 @@ class MutationDict(Mutable, dict):
 
         dict.__delitem__(self, key)
         self.changed()
+
+
+class MutableList(Mutable, list):
+    @classmethod
+    def coerce(cls, key, value):
+        if not isinstance(value, MutableList):
+            if isinstance(value, list):
+                return MutableList(value)
+            value = Mutable.coerce(key, value)
+
+        return value
+
+    def __setitem__(self, key, value):
+        old_value = list.__getitem__(self, key)
+        for obj, key in self._parents.items():
+            old_value._parents.pop(obj, None)
+
+        list.__setitem__(self, key, value)
+        for obj, key in self._parents.items():
+            value._parents[obj] = key
+
+        self.changed()
+
+    def append(self, item):
+        list.append(self, item)
+        self.changed()
+
+    def extend(self, iterable):
+        list.extend(self, iterable)
+        self.changed()
+
+    def insert(self, index, item):
+        list.insert(self, index, item)
+        self.changed()
+
+    def remove(self, value):
+        list.remove(self, value)
+        self.changed()
+
+    def reverse(self):
+        list.reverse(self)
+        self.changed()
+
+    def pop(self, index=-1):
+        item = list.pop(self, index)
+        self.changed()
+        return item
+
+    def sort(self, cmp=None, key=None, reverse=False):
+        list.sort(self, cmp, key, reverse)
+        self.changed()
+
+    def __getstate__(self):
+        return list(self)
+
+    def __setstate__(self, state):
+        self[:] = state
