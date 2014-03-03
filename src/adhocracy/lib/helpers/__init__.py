ffb118d840e0f2fca64bfb664b1d2d5be0b6ca75
Nicolas Dietrich <nicodietrich@gmail.com>
2014-03-03 13:44:51 +0100

    pep8

--- src/adhocracy/lib/helpers/__init__.py
+++ src/adhocracy/lib/helpers/__init__.py
@@ -193,7 +193,7 @@ def get_redirect_url(target=u'login', entity=None, **kwargs):
     if c.came_from == u'':
         if entity is None:
             c.came_from = base_url(request.path,
-                                 query_string=request.query_string)
+                                   query_string=request.query_string)
         else:
             c.came_from = entity_url(entity, **kwargs)
 
