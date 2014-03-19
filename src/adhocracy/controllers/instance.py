liqd/adhocracy

commit 6693c292d19d243cc90dd25da6abac75c4223475

implement event/all filter API for instance activity

src/adhocracy/controllers/instance.py
@@ -327,7 +327,11 @@ class InstanceController(BaseController):
         if format == 'sline':
             ret_abort(u'Sparkline data is not available anymore.', code=410)
 
-        events = model.Event.find_by_instance(c.page_instance, limit=50)
+        events = model.Event.all_q(
+            include_hidden=False,
+            event_filter=request.params.getall('event_filter'))\
+            .order_by(model.Event.time.desc())\
+            .limit(min(int(request.params.get('count', 50)), 100)).all()
 
         if format == 'rss':
             return event.rss_feed(events,

commit e59b130fe5a88f8e6a6ab9e6376d855457afbbe8

regression: filter instance activity by instance
regression from 6693c29

src/adhocracy/controllers/instance.py
@@ -332,7 +332,7 @@ class InstanceController(BaseController):
             ret_abort(u'Sparkline data is not available anymore.', code=410)
 
         events = model.Event.all_q(
-            include_hidden=False,
+            instance=c.page_instance,
             event_filter=request.params.getall('event_filter'))\
             .order_by(model.Event.time.desc())\
             .limit(min(int(request.params.get('count', 50)), 100)).all()