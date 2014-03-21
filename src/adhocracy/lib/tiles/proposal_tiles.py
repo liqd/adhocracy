d44c3deb17ba9734cb19afb9bcb2a880bad2dd56
Joscha Krutzki <joka@jokasis.de>
2014-03-21 20:30:31 +0100

    UL: Hack: disable caching for propsal tile row_inline

--- src/adhocracy/lib/tiles/proposal_tiles.py
+++ src/adhocracy/lib/tiles/proposal_tiles.py
@@ -65,7 +65,7 @@ def row_inline(proposal):
         return ""
     return render_tile('/proposal/tiles.html', 'row_inline',
                        ProposalTile(proposal),
-                       proposal=proposal, cached=True,
+                       proposal=proposal, cached=False,
                        badgesglobal_admin=global_admin)
 
 
