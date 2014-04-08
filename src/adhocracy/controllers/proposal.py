56cc129673aad0917f5859d9ddbf045151307a8c
Tobias Bengfort <tobias.bengfort@liqd.de>
2014-04-08 22:44:43 +0200

    fixup ret_url to came_from

--- src/adhocracy/controllers/proposal.py
+++ src/adhocracy/controllers/proposal.py
@@ -463,10 +463,10 @@ class ProposalController(BaseController):
         c.page = c.proposal.description
 
         if format == u'overlay':
-            c.ret_url = h.entity_url(c.proposal,
-                                     member='comments',
-                                     in_overlay=False,
-                                     format='overlay')
+            c.came_from = h.entity_url(c.proposal,
+                                       member='comments',
+                                       in_overlay=False,
+                                       format='overlay')
             return render("/page/comments.html", overlay=True,
                           overlay_size=OVERLAY_SMALL)
         else:
